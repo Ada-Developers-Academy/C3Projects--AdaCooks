02475894074085f5e5807866bef1ebfaa1257dd2
MisShellyMac <michellemccarthy@hotmail.com>
2015-07-27 16:56:07 -0700

    Added Secure Password field to User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  #Associations
+  has_secure_password
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :new
+    end
+  end
 end
