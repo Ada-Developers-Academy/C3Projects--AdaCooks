7bc706b8ae7b8bc7c681ad11fdd2f0bb0f0c590e
amirahaile <amira.dhaile@gmail.com>
2015-07-27 14:42:31 -0700

    Specs for controllers and models.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  # test associations?
+
+  context "creating a new user" do
+    describe "validates" do
+      # username is unique
+      # email is unique
+    end
+  end
+end
