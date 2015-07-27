7e625b75168d14af93193f43d81c82055db486d0
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-27 15:09:00 -0700

    added recipes_cookbooks join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727215033) do
+ActiveRecord::Schema.define(version: 20150727220754) do
 
   create_table "cookbooks", force: :cascade do |t|
     t.string   "name",        null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20150727215033) do
     t.integer  "user_id",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "cookbooks_recipes", id: false, force: :cascade do |t|
+    t.integer "recipe_id",   null: false
+    t.integer "cookbook_id", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
