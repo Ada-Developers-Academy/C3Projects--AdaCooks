f98834fc570b3f2385bcc5c7575350ad56d93be4
amirahaile <amira.dhaile@gmail.com>
2015-07-27 14:41:09 -0700

    Fixed typo in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   root 'user#index'
 
-  resources :user, :recipe, :ingredient, :cookbook
+  resources :users, :recipes, :ingredients, :cookbooks
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
