774414b318c656254c5179d1c9bfa804b19eb5b4
Anita <asovay@gmail.com>
2015-07-27 16:21:14 -0700

    added user validations and started user specs for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    it "requires a name to be present" do
+      user = create :user, name: nil
+
+      expect(user).to be_invalid
+      expect(user.errors.keys).to include(:name)
+    end
+  end
 end
