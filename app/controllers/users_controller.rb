083de618342486ae3c638e41f006e197cd55b11f
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-27 19:49:27 -0700

    WIP: home page and user dashboard views.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def show
+    @user = User.find(id: params[:id])
+  end
+
+  def index
+
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
