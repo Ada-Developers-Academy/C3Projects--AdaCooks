06526cffbee20072782a2981c8658dbe253e5552
amirahaile <amira.dhaile@gmail.com>
2015-07-28 10:18:55 -0700

    Added routes for sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'user#index'
 
   resources :users, :recipes, :ingredients, :cookbooks
+  resources :session, only: [:create, :destroy]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
