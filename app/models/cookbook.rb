3026981ef98e24096b7689d48b7121923bf6d1b9
Elia <elgrenier@gmail.com>
2015-07-27 23:52:33 -0700

    added validations to the models. one on recipes needs to be added still -one or more ingredients

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -1,5 +1,10 @@
 class Cookbook < ActiveRecord::Base
+# Associations --------------------------------------------------
   belongs_to :user
   has_and_belongs_to_many :recipes
   has_many :ingredients, through: :recipes
+
+# Validations ---------------------------------------------------
+  validates :name, presence: true
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+# Associations --------------------------------------------------
   has_secure_password
-
   has_many :cookbooks
   has_many :recipes
-  
+
+# Validations ---------------------------------------------------
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
