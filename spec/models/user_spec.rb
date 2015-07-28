17a617367fc39c950f49a1a60a582756d1357760
Elsa <elsa.moluf@gmail.com>
2015-07-28 14:20:27 -0700

    Finished initial model tests. Recipes model still requires image testing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,34 +8,67 @@ RSpec.describe User, type: :model do
       expect(User.count).to eq(1)
     end
 
-    it "doesn't create invalid users" do
+    it "requires a name" do
       user = build :user, name: nil
 
       expect(user).to_not be_valid
       expect(user.errors.keys).to include(:name)
+    end
+
+    it "requires a unique name" do
+      create :user, name: "bob"
+      bob2 = build :user, name: "bob"
+
+      expect(bob2).to_not be_valid
+      expect(bob2.errors.keys).to include(:name)
+    end
+
+    it "requires an email" do
+      user = build :user, email: nil
+
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include(:email)
+    end
+
+    it "requires a unique email" do
+       create :user, email: "haha@lol.com"
+       joker = build :user, email: "haha@lol.com"
+
+      expect(joker).to_not be_valid
+      expect(joker.errors.keys).to include(:email)
+    end
+
+    it "requires a password" do
+      user = build :user, password: nil
+
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include(:password)
     end
   end # validations
 
   describe "associations" do
     it "has many cookbooks" do
       user = create :user, id: 1
-      create :cookbook, user_id: 1
+      healthy = create :cookbook, name: "healthy", user_id: 1
+      unhealthy = create :cookbook, name: "unhealthy", user_id: 1
 
-      expect(user.cookbooks.count).to eq(1)
+      expect(user.cookbooks).to include(healthy, unhealthy)
     end
 
     it "has many recipes" do
       user = create :user, id: 1
-      create :recipe, user_id: 1
+      stone_soup = create :recipe, name: "stone soup", user_id: 1
+      earth_worms = create :recipe, name: "earth worms", user_id: 1
 
-      expect(user.recipes.count).to eq(1)
+      expect(user.recipes).to include(earth_worms, stone_soup)
     end
 
     it "has many ingredients" do
       user = create :user, id: 1
-      create :ingredient, user_id: 1
+      h2o = create :ingredient, name: "water", user_id: 1
+      co = create :ingredient, name: "carbon monoxide", user_id: 1
 
-      expect(user.ingredients.count).to eq(1)
+      expect(user.ingredients).to include(h2o, co)
     end
 
   end # associations
