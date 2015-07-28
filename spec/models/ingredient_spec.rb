9db5307d1637ec0c41bca221432ae8c3d20feaeb
Carly Jugler <carjug@gmail.com>
2015-07-28 14:15:32 -0700

    Adds all rspecs for ingredient model attributes, associations, and validations.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -23,10 +23,29 @@ RSpec.describe Ingredient, type: :model do
         expect(@ingredient.user_id).to eq(1)
       end
 
+      it "is associated with a recipe" do
+        recipe = create :recipe, name: "A recipe", ingredients: [@ingredient]
+
+        expect(@ingredient.recipes).to include(recipe)
+      end
     end
 
     describe "model validations" do
+      it "has to have a name" do
+        ingredient = build :ingredient, name: nil
 
+        expect(ingredient).to_not be_valid
+        expect(ingredient.errors.keys).to include(:name)
+      end
+
+      it "name has to be unique" do
+        ingredient1 = create :ingredient, name: "An ingredient"
+
+        ingredient2 = build :ingredient, name: "An ingredient"
+
+        expect(ingredient2).to_not be_valid
+        expect(ingredient2.errors.keys).to include(:name)
+      end
     end
 
   end
