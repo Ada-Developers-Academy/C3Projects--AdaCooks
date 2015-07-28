d923d8f12d6c091cba9fbd4cd9132b6724d29f68
Kari <kari.lonczak@gmail.com>
2015-07-28 16:54:52 -0700

    Added missing ingredient image to folder. Created seed program to read our seed_data_ingredients.csv file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'CSV'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CSV.foreach("db/seed_data_ingredients.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    image_path = "app/assets/images/" + row[:image]
+
+    Ingredient.create(
+      name: row[:name],
+      image: open(image_path)
+      )
+end
