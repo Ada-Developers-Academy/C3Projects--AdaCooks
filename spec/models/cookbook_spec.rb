fc8df71d4c97bf62591cc86231f6ccd4c3c87c96
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-28 13:36:19 -0700

    Added specs for model validations

--- spec/models/cookbook_spec.rb
+++ spec/models/cookbook_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cookbook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    it "requires a name" do
+      cookbook = build :cookbook, name: nil
+
+      expect(cookbook).to_not be_valid
+      expect(cookbook.errors.keys).to include(:name)
+    end
+
+    it "must be associated with a user" do
+      cookbook = build :cookbook, user_id: nil
+
+      expect(cookbook).to_not be_valid
+      expect(cookbook.errors.keys).to include(:user_id)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    describe "username" do
+      it "requires a username" do
+        user = build :user, username: nil
+
+        expect(user).to_not be_valid
+        expect(user.errors.keys).to include(:username)
+      end
+
+      it "requires username to be unique" do
+        user = create :user
+        user1 = build :user, email: "sam@sam.com"
+
+        expect(user1).to_not be_valid
+        expect(user1.errors.messages).to include(:username => ["has already been taken"])
+      end
+    end
+
+    describe "email" do
+      it "requires an email" do
+        user = build :user, email: nil
+
+        expect(user).to_not be_valid
+        expect(user.errors.keys).to include(:email)
+      end
+
+      it "requires email to be unique" do
+        user = create :user
+        user1 = build :user, username: "sam"
+
+        expect(user1).to_not be_valid
+        expect(user1.errors.messages).to include(:email => ["has already been taken"])
+      end
+    end
+
+    it "requires a password" do
+      user = build :user, password: nil
+
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include(:password)
+    end
+  end
 end
