d07fed44f4b240ef176933c1f8c7265a087b4b20
Shanna Chau <shannachau@gmail.com>
2015-07-28 14:42:01 -0700

    Set up basic views for recipes index and show pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,24 @@
 class RecipesController < ApplicationController
-  def index; end
-  def new; end
-  def create; end
-  def edit; end
-  def update; end
-  def show; end
-  def destroy; end
+  def index
+    @recipes = Recipe.all.order(:name)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+  end
 end
