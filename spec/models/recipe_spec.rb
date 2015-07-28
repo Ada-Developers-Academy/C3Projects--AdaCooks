c40ea469900b19d9f46970379682e027c0be0570
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-27 17:54:35 -0700

    refactored uniqueness specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -8,17 +8,21 @@ RSpec.describe Recipe, type: :model do
       expect(recipe.errors.keys).to include :name
     end
 
-    it "requires that a name be unique" do
-      recipe = create :recipe
-      recipe2 = build :recipe
-      expect(recipe2).to_not be_valid
-      expect(recipe2.errors.keys).to include :name
-    end
+    context "name uniqueness" do
+      before :each do
+        create :recipe
+      end
 
-    it "allows a recipe with a unique name" do
-      recipe = create :recipe
-      recipe2 = build :recipe, name: "sweet cream ice cream"
-      expect(recipe2.errors.keys).to_not include :name
+      it "requires that a name be unique" do
+        recipe = build :recipe
+        expect(recipe).to_not be_valid
+        expect(recipe.errors.keys).to include :name
+      end
+
+      it "allows a recipe with a unique name" do
+        recipe = build :recipe, name: "sweet cream ice cream"
+        expect(recipe.errors.keys).to_not include :name
+      end
     end
   end
 end
