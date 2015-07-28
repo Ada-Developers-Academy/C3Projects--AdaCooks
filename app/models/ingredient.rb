64269e2b9a6741341424ad2ef8049f6ff2e289e0
Elia <elgrenier@gmail.com>
2015-07-27 23:41:12 -0700

    wip: working on adding carrierwave, don't want to get too far ahead or off base alone since this is all new

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,4 @@
 class Ingredient < ActiveRecord::Base
   has_and_belongs_to_many :recipes
+  mount_uploader :image, IngredientsUploader
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :ingredients
   has_and_belongs_to_many :cookbooks
+  mount_uploader :image, RecipesUploader
 end
