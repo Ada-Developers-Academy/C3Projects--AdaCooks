00dadaad515218cbd695caa6f58b3af4e4b65171
amirahaile <amira.dhaile@gmail.com>
2015-07-27 18:01:19 -0700

    Added helper method for form errors.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+	def error_check(attribute)
+		if @user.errors[attribute].any?
+			flash.now[(attribute.to_s + "_error").to_sym] = (@user.errors.messages[attribute][0].capitalize + ".")
+		end
+	end
 end
