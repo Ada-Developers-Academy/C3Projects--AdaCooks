a04e81497e0fdc63359a2af9baa7ab9026a2c926
Anita <asovay@gmail.com>
2015-07-27 22:10:17 -0700

    implicit parens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,11 @@ private
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
+    redirect_to root_url unless current_user?(@user)
   end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
