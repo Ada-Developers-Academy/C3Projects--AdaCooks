69d6f50c911bd33a49d0b81b2079372df2cb0704
Victoria <vikshab2010@hotmail.com>
2015-07-28 14:50:01 -0700

    Created methods in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user
+    #  redirect to somewhere
+    else
+      flash.now[:error] = "Incorrect Username/password combination, Please Try again"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
