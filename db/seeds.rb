1f82d6f346016869be136d88649bc9ca02296b35
Victoria <vikshab2010@hotmail.com>
2015-07-27 17:44:07 -0700

    Added images for ingredients & recipes Created csv file for user, ingredients & recipes Seed -WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CSV.foreach("db/users.csv"), headers: true) do |row|
+
+  User.create(
+  name: row[0],
+  email: row[1],
+  password: row[2],
+  password_confirmation: row[3]
+  )
+end
+
+CSV.foreach("db/ingredients.csv", headers: true) do |row|
+
+  Ingredient.create(
+  name: row[0],
+  image_url: row[1]
+  )
+end
+
+CSV.foreach("db/recipes.csv", headers: true) do |row|
+
+  Recipe.create(
+  name: row[0],
+  description: row[1],
+  image_url: row[2],
+  preparation: row[3],
+  cookbook_id: row[4]
+  )
+end
