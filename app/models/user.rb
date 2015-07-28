7bfb4f0c67b449e383cc68e16ef59cffaf266d28
Elia <elgrenier@gmail.com>
2015-07-28 14:51:20 -0700

    added a users column to ingredients

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :cookbooks
   has_many :recipes
+  has_many :ingredients
 
 # Validations ---------------------------------------------------
   validates :username, presence: true, uniqueness: { case_sensitive: false }
