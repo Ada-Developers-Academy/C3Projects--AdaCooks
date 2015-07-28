db8e505544b932717f7bb94f4a3843e53316381d
Anita <asovay@gmail.com>
2015-07-27 17:02:49 -0700

    added model tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ActiveRecord::Base
   # VALIDATIONS ---------------------------------------
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name,      presence: true, length: { maximum: 50 }
-  validates :email,     presence: true, length: { maximum: 255 }, 
+  validates :name,      presence: true
+  validates :email,     presence: true,
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
   validates :password,  presence: true, length: { minimum: 6 }
