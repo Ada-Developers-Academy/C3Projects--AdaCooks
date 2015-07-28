d51455ba1b1424e690445a633d4035ccf45f0701
amirahaile <amira.dhaile@gmail.com>
2015-07-27 17:49:51 -0700

    Set up factory_girl for model testing and spec'd validations.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
+
   factory :ingredient do
     name "An ingredient"
     image ""
   end
+
+	factory :user do
+		username 'Toastee'
+		email 'toastee@flavaflav.com'
+		password 'jentoof'
+		password_confirmation 'jentoof'
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,44 @@ RSpec.describe User, type: :model do
 
   context "creating a new user" do
     describe "validates" do
-      # username is unique
-      # email is unique
+
+      it "username is required" do
+        user = build :user, username: nil
+
+        expect(user).to_not be_valid
+        expect(user.errors.keys).to include(:username)
+      end
+
+      it "email is required" do
+        user = build :user, email: nil
+
+        expect(user).to_not be_valid
+        expect(user.errors.keys).to include(:email)
+      end
+
+      it "password is required" do
+        user = build :user, password: nil
+
+        expect(user).to_not be_valid
+        expect(user.errors.keys).to include(:password)
+      end
+
+      it "password_confirmation is required" do
+        user = build :user, password_confirmation: nil
+
+        expect(user).to_not be_valid
+        expect(user.errors.keys).to include(:password_confirmation)
+      end
+
+      it "username and email are both unique" do
+        create :user
+        user2 = build :user
+
+        expect(user2).to_not be_valid
+        expect(user2.errors.keys).to include(:username)
+        expect(user2.errors.keys).to include(:email)
+        expect(User.all.count).to eq 1
+      end
     end
   end
 end
