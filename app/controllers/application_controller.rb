cb23007a6af95b482074ea8cd951f008e9d2c86e
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 16:05:58 -0700

    created #authorize and #current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  MESSAGES = {
+    not_signed_in: "Please sign in to access that page.",
+    signed_in: "Sign in successful. Enjoy your ice cream!"
+  }
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def authorize
-    redirect_to signin_path unless current_user
+    if current_user
+      flash[:success] = MESSAGES[:signed_in]
+    else
+      redirect_to signin_path
+      flash[:errors] = MESSAGES[:not_signed_in]
+    end
   end
 end
