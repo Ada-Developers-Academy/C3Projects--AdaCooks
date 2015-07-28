0c5a1684c2699d011736bf0f256ec04d0ba3a116
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 14:58:37 -0700

    created spec for #index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  describe "get #index" do
+    before :each do
+      get :index
+    end
 
+    it "responds with an HTTP 200 status" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template("index")
+    end
+  end
 end
