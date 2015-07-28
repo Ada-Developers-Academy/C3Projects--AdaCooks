4f13ca1881b1b8f5ba6b9bdfe92391ff07b1b446
amirahaile <amira.dhaile@gmail.com>
2015-07-27 17:49:11 -0700

    Created and spec'd form for registering users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,24 @@ class UsersController < ApplicationController
 	def index; end
 
 	def new
+		# guard against logged in users from registering
 		@user = User.new
+	end
+
+	def create
+		@user = User.new(create_params)
+
+		if @user.save
+			session[:user_id] = @user.id  # now logged in
+			redirect_to user_path(@user)
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def create_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
 	end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+	describe "#create" do
+		it "instantiates a new User object" do
+			user = create :user
+			expect(User.all).to include(user) # `expect(user).to exist` failed - why?
+		end
 
+		it "redirects to user's page if information is valid" do
+			user_params = { :user => { name: 'Toastee', email: 'toastee@flavaflav.com', password: 'jentoof', password_confirmation: 'jentoof' } }
+			post :create, user_params
+
+			expect(subject).to redirect_to(:action => :show, :id => assigns(:user).id)
+			# error: Expected response to be a <redirect>, but was <200> - whyy?
+		end
+
+		it "renders :new view if the information is invalid" do
+			user_params = { :user => { name: 'Toastee', email: nil } }
+			post :create, user_params
+
+			expect(subject).to render_template(:new)
+		end
+	end
 end
