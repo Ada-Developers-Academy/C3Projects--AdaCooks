f59b35f2a2e691e8ca2dee1e4af23f8c14990f71
Elsa <elsa.moluf@gmail.com>
2015-07-28 15:01:35 -0700

    Set up basic routes and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +14,18 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :users, only: [:new, :create]
+  # custom dashboard
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :cookbooks
+
+  resources :recipes
+
+  resources :ingredients
+
 
   # Example resource route with options:
   #   resources :products do
