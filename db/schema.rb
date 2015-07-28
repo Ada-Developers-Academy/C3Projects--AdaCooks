97d916dc7a8e46774d855767281138a02f55c3a7
MisShellyMac <michellemccarthy@hotmail.com>
2015-07-27 17:02:26 -0700

    Added column to cookbooks to include user_id foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727215101) do
+ActiveRecord::Schema.define(version: 20150727235925) do
 
   create_table "cookbooks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
