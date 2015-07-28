babb480fcc6b00f626429a9b6de1582ce70eb7d3
Loren Wang <wang131.131@gmail.com>
2015-07-28 11:58:36 -0700

    deleted

--- app/controllers/recipes_controller.rb
+++
@@ -1,5 +0,0 @@
-class RecipesController < ApplicationController
-  def index
-    @recipes = Recipe.all
-  end
-end
--- app/models/recipe.rb
+++
@@ -1,3 +0,0 @@
-class Recipe < ActiveRecord::Base
-  has_and_belongs_to_many :ingredients
-end
