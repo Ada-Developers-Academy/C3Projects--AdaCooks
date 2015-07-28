591c7565ce46dd8521e77ced0dfd1171164b4bab
Shanna Chau <shannachau@gmail.com>
2015-07-28 15:54:27 -0700

    Finished recipes controller tests for get index and get show

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
+  let(:recipe) { create :recipe }
+
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
@@ -8,14 +10,26 @@ RSpec.describe RecipesController, type: :controller do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+
+    it "renders the index template" do
+      get :index
+
+      expect(response).to render_template("index")
+    end
   end
 
   describe "GET #show" do
     it "responds successfully with an HTTP 200 status code" do
-      get :show, id: 1
+      get :show, id: recipe
 
       expect(response).to be_success
       expect(response).to have_http_status(200)
+    end
+
+    it "renders show template" do
+      get :show, id: recipe
+
+      expect(response).to render_template("show")
     end
   end
 end
