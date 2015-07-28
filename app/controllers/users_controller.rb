f0175beb155ea675166e4ea1e8b8a1ed5506822c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 15:21:14 -0700

    wrote specs for invalid user params on #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
+      # redirect to dashboard_path(@user)
       redirect_to root_path
     else
       render :new
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,13 +16,34 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "post #create" do
-    let(:valid_params) do
-      { username: "ren", password: "1111", password_confirmation: "1111" }
+    context "valid params" do
+      let(:valid_params) do
+        { username: "ren", password: "1111", password_confirmation: "1111" }
+      end
+
+      it "creates a new user" do
+        post :create, :user => valid_params
+        expect(User.count).to eq 1
+      end
     end
 
-    it "creates a new user" do
-      post :create, :user => valid_params
-      expect(User.count).to eq 1
+    # write test for redirect to user#dashboard when dashboard is created
+    context "invalid params" do
+      let(:invalid_params) do
+        { username: "stimpy", password: "1111", password_confirmation: "1234" }
+      end
+
+      before :each do
+        post :create, :user => invalid_params
+      end
+
+      it "does not create a new user" do
+        expect(User.count).to eq 0
+      end
+
+      it "renders the new template" do
+        expect(response).to render_template("new")
+      end
     end
   end
 end
