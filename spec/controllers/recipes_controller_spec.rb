8cc48e21aac71e38d6a751bcc5b7dd551a525ad7
Ashley Watkins <AWatkins31@gmail.com>
2015-07-28 16:55:05 -0700

    Adds tests for recipes#index

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
+  let(:all_recipes) { Recipe.all }
 
+  describe "get :index" do
+    before :each do
+      5.times { create(:recipe) }
+    end
+
+    it "assigns @recipes" do
+      get :index
+
+      expect(assigns(:recipes).count).to eq(5)
+      expect(assigns(:recipes)).to eq(all_recipes)
+    end
+
+    it "renders the index template" do
+      get :index
+
+      expect(response).to render_template("index")
+    end
+  end
 end
