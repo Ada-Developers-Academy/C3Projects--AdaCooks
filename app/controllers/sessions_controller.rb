a19bbd66abd228ca9ecde9370bcbff790954e1fd
amirahaile <amira.dhaile@gmail.com>
2015-07-28 15:07:24 -0700

    Spec'd and wrote logging in functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+
+	def create # logging in
+		if identify_user && authenticate
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
+		end
+	end
+
+	private
+
+	def identify_user
+		if User.find_by(username: params[:session][:username])
+			@user = User.find_by(username: params[:session][:username])
+			return true
+		else
+			redirect_to new_user_path
+			flash[:notice] = "We couldn't find an account with that username.\nWhy not create a new account?"
+			return # exits the action
+		end
+	end
+
+	def authenticate
+		if @user && @user.authenticate(params[:session][:password])
+			return true
+		else
+			redirect_to sessions_path
+			flash[:error] = "Sorry, your username or password did not match our records."
+			return # exists the action
+		end
+	end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+	describe "POST #create" do
+		# don't need to `create :user` because it's already is in the db??
 
+		let(:params) { { :session => { username: 'Toastee', password: 'jentoof' } } }
+		let(:bad_params) { { :session => { username: 'Hemingway', password: 'havana' } } }
+		let(:bad_password_params) { { :session => { username: 'Toastee', password: 'flav' } } }
+
+		describe "#identify_user" do
+			context "the user has an account" do
+				it "the user's account is assigned to @user" do
+					post :create, params
+					expect(assigns(:user)).to eq User.find_by(username: 'Toastee')
+				end
+			end
+
+			context "the user does not have an account" do
+				it "the user is redirected to the register page" do
+					post :create, bad_params
+					expect(subject).to redirect_to(new_user_path)
+				end
+
+				it "flash errors are sent regarding the error" do
+					post :create, bad_params
+					expect(flash[:notice]).to_not be nil
+				end
+			end
+		end
+
+		describe "#authenticate" do
+			context "when credentials aren't authenticated" do
+				it "the user is redirected back to the login page" do
+					post :create, bad_password_params
+					expect(subject).to redirect_to(sessions_path)
+				end
+
+				it "flash errors are sent regarding the error" do
+					post :create, bad_password_params
+					expect(flash[:error]).to_not be nil
+				end
+			end
+		end
+
+		context "when credentials are authenticated" do
+			it "assign user_id to the session" do
+				post :create, params
+				expect(session[:user_id]).to eq 1
+			end
+
+			it "the user is redirected to their page" do
+				post :create, params
+				expect(subject).to redirect_to(:controller => :users, :action => :show, :id => 1)
+			end
+		end
+	end
 end
