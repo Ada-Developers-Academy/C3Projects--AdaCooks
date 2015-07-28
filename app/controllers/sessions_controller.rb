c00c53fb3a7e8d3d7a9c3e2f14a0c7de8a380440
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-28 16:48:44 -0700

    WIP for sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:session][:password]
+    session[:user_id] = params[:user][]
+  end
+
+  def destory
+  end
+
+  def authenticated?
+    if params[:session][:password] == User.find_by(email: params[:session][:email]).password
+      return true
+    else
+      return false
+    end
+  end
+
 end
