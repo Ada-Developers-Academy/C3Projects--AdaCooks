4b8064de63d65b68b6a92bbc5fdd2e4206379eb1
Katie Atrops <katrops@gmail.com>
2015-07-28 16:11:28 -0700

    added belongs_to user association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ActiveRecord::Base
   #Associations------------------------------------------------------------------
   has_many :ingredients
   has_and_belongs_to_many :cookbooks
+  belongs_to :user
   #Validations-------------------------------------------------------------------
   validates :name, presence: true
   # validates :description
