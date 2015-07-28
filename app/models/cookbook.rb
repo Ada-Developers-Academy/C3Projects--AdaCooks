ed4eee5778fc5195fbe8e7320bd6d6ca7ddce9ca
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-27 21:11:59 -0700

    created spec and validation for :name presence

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -1,4 +1,8 @@
 class Cookbook < ActiveRecord::Base
+  # Associations ---------------------------------------------------------------
   has_and_belongs_to_many :recipes
   belongs_to :user
+
+  # Validations ----------------------------------------------------------------
+  validates :name, presence: true
 end
--- spec/models/cookbook_spec.rb
+++ spec/models/cookbook_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Cookbook, type: :model do
+  context "validations" do
+    it "requires that a name be present" do
+      cookbook = build :cookbook, name: nil
+      cookbook.valid?
+      expect(cookbook.errors.keys).to include :name
+    end
+  end
 end
