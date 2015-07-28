36925790aa6522aeff9643149145fbd99d49ec70
Corinne Pingul <corinnepingul@gmail.com>
2015-07-28 15:42:42 -0700

    Added a test for index method.

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
+  describe "GET #index" do
+    before :each do
+      %w(Happy Lovely Pig Darling Appetite Bumpkin).each do |name|
+        create(:recipe, name: name)
+      end
+    end
 
+    it "instantiates a variable that is all the recipes in alpha_order" do
+      get :index
+
+      expect(response.status).to eq 200
+      expect(assigns(:recipes).first.name).to eq "Appetite"
+    end
+
+    it "renders the recipes#index view" do
+      get :index
+
+      expect(subject).to render_template :index
+    end
+  end
 end
