9f639a08508a8d7d4834a85313becf920f0e8ee0
Lila Garcia <lilagrc@gmail.com>
2015-07-28 15:43:40 -0700

    Added table of user cookbooks with numbers of recipes and ingredients

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -2,7 +2,8 @@ class Cookbook < ActiveRecord::Base
 
 # associations ________________________________________________________________
   belongs_to :user
-  has_many :ingredients, through: :recipes
+  has_many :recipes
+  has_many :ingredients, -> { uniq }, through: :recipes
 
 # Validations ________________________________________________________________
   validates :name, presence: true
