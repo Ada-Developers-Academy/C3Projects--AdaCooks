5332789b419c4a4d65039d67633cb6b5610ede19
Jeri <drvonnjerryxlii@gmail.com>
2015-07-29 08:27:13 -0700

    added basic user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
-  def show; end
+  before_action :set_user, only: :show
+
+  def show; end # FIXME: test users#show
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
