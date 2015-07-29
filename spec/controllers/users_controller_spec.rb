623bb8ab4fcdb40e534df85bf89ae0b3c4495f11
Corinne Pingul <corinnepingul@gmail.com>
2015-07-29 15:56:42 -0700

    Deletes users#index functionality because we don't need it.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,26 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET #index" do
-    it "renders the index view" do
-      get :index
-
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-      expect(subject).to render_template(:index)
-    end
-
-    it "assigns @users instance variable" do
-      %w(user1 user2 user3).each do |username|
-        create(:user, username: username)
-      end
-
-      get :index
-
-      expect(assigns(:users).count).to eq 3
-    end
-  end
-
   describe "GET #new" do
     before :each do
       get :new
