6d6049f44238b2419c1bb404d0e8942dd164fe05
Katie Atrops <katrops@gmail.com>
2015-07-29 16:35:36 -0700

    models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,7 @@
 class Ingredient < ActiveRecord::Base
   #Associations------------------------------------------------------------------
   has_and_belongs_to_many :recipes
+  belongs_to :user
   #Validations-------------------------------------------------------------------
   validates :name, presence: true, uniqueness: true
   # validates :image
