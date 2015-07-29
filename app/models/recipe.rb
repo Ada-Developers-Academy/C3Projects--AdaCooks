129e3c1c82a3c41a74612dac931fe6dce801cccd
amirahaile <amira.dhaile@gmail.com>
2015-07-29 16:03:40 -0700

    Created the form to make a new recipe using NESTED FORMS. Thanks TA Jeremy 🙌🏾

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,11 @@
 class Recipe < ActiveRecord::Base
+	mount_uploader :image_url, ImageUploader
+
 # Associatons ------------------------------------------------------------------
 	belongs_to :cookbook
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
+	accepts_nested_attributes_for :recipe_ingredients
 
 # Validations ------------------------------------------------------------------
 	validates :name, :description, :prep, presence: true
