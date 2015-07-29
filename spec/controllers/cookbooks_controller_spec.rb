5db25660d0b32df8f692aa60a4c0a774903108a9
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-29 15:53:30 -0700

    Added tests for #show.

--- spec/controllers/cookbooks_controller_spec.rb
+++ spec/controllers/cookbooks_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CookbooksController, type: :controller do
+  describe "GET #show" do
+    before :each do
+      cookbook = create :cookbook
+      user = create :user
+      session[:user_id] = user.id
+      get :show, id: cookbook.id
+    end
 
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the show view" do
+      expect(response).to render_template("show")
+    end
+  end
 end
