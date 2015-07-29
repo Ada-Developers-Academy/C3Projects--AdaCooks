94679624298a11d4a9175f3349735ac5cc10eb00
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-29 11:31:30 -0700

    Changed method name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :logged_in_user
 
   # if trying to access a location as a non-logged in user
   def require_login
@@ -11,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   # to make an accessible user variable for the header, if logged in
-  def logged_in_user
+  def set_logged_in_user
     @user = User.find(session[:user_id]) unless session[:user_id].nil?
   end
 end
