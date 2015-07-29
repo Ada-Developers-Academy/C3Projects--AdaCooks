360672986f226d8f362d51a883b51d6b76540b1b
Jeri <drvonnjerryxlii@gmail.com>
2015-07-28 21:14:30 -0700

    added more flavor text to the header nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#root'
   resources :users, only: [:show]
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :new]
   resources :ingredients, only: [:index, :show]
 end
