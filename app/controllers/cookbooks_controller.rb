ac69df3c4cf92d63320c50ec8e267decf796f9c4
Shanna Chau <shannachau@gmail.com>
2015-07-29 13:57:52 -0700

    Added functionality for user to add new cookbook

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -2,13 +2,18 @@ class CookbooksController < ApplicationController
   before_action :require_user_login
 
   def index
-    @cookbooks = User.find(params[:user_id]).cookbooks
+    @user = User.find(params[:user_id])
+    @cookbooks = @user.cookbooks
   end
 
   def new
+    @cookbook = Cookbook.new
   end
 
   def create
+    @cookbook = Cookbook.create(create_params)
+
+    redirect_to user_cookbooks_path(session[:user_id])
   end
 
   def edit
@@ -24,4 +29,12 @@ class CookbooksController < ApplicationController
 
   def destroy
   end
+
+  private
+    def create_params
+      create_params = params.require(:cookbook).permit(:name, :description, :user_id)
+      create_params[:user_id] = session[:user_id]
+
+      return create_params
+    end
 end
