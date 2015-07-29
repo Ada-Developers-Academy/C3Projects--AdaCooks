0b053f2d29eb56e116785d38b2d7f57b59bc1953
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-28 19:00:36 -0700

    WIP: rspec test for ingredient validation not working properly.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
+
 RSpec.describe Ingredient, type: :model do
   context "name validation" do
     let(:pine) {create :ingredient}
+    # + case
+    it "creates ingredient if name is present" do
+      expect(pine).to be_valid
+    end
 
+    # - case
     it "requires ingredient name to be present" do
       pine = build :ingredient, name: nil
 
@@ -14,8 +20,8 @@ RSpec.describe Ingredient, type: :model do
     it "requires a name to be unique" do
       pine
       pine2 = build :ingredient
-
       expect(pine2.errors.keys).to include(:name)
+      # I don't know why above isn't working
       expect(pine2).to_not be_valid
     end
   end
