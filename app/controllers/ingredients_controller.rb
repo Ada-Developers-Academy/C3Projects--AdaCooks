403435eb4262e68e429c2044db84d46a4c42fc34
Elia <elgrenier@gmail.com>
2015-07-29 14:27:38 -0700

    wip: specs for logged in ingredient functionality.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,11 +1,38 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
+  before_action :require_login, only: [:new, :create]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @ingredients = Ingredient.alpha_order
   end
 
-  def show
-    @ingredient = Ingredient.find(params[:id])
+  def show; end
+
+  def new
+    @ingredient = Ingredient.new
   end
 
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+      def create_ingredient
+        @ingredient = Ingredient.new
+      end
+
+      def set_ingredient
+        @ingredient = Ingredient.find(params[:id])
+      end
+
+      def ingredient_params
+        params.require(:ingredient).permit(:name, :image, :user_id)
+      end
 end
--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -17,20 +17,41 @@ RSpec.describe IngredientsController, type: :controller do
     end
   end
 
+  describe "Logging in creates more access" do
 
+    context "you can't access #new anonymously" do
+      it "get #new redirects anonymous user to home" do
+        get :new
 
-    # before :each do
-    #   user = User.create(name: "first_user", email: "name@email.com", password_digest: "foobar")
-    #   session[:user_id] = @user.id
-    # end
-    # it "renders the #show view" do
-    #   get :show, id: @user.id
-    #   expect(response).to render_template("show")
-    # end
-    #
-    # it "renders the #new view" do
-    #   get :new, user_id: @user
-    #   expect(response).to render_template("new")
-    # end
+        expect(response).to redirect_to root_path
+        expect(response).to have_http_status(302)
+      end
+    end
 
+    context "Logged in users can access #new" do
+
+      it "renders the #new view" do
+        user = create :user
+        session[:user_id] = user.id
+        get :new, user_id: user
+
+        expect(response).to render_template("new")
+      end
+    end
+
+    context "Logged in users can only edit and delete their own ingredients" do
+      before :each do
+        @logged_in_user = create :user
+        @other_user = create :user, username: "other_user"
+        session[:user_id] = @logged_in_user.id
+        @ingredient = create :ingredient
+      end
+
+      it "renders the #edit view" do
+        get :edit, id: @logged_in_user.id
+
+        expect(response).to render_template("edit")
+      end
+    end
+  end
 end
