3509145cc0c1ec9a98f27d66b9671ff3434197ee
Loraine <loraine@lorainekv.com>
2015-07-29 11:25:49 -0700

    Update method.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update, :show, :destroy]
+
 
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -23,7 +24,21 @@ class RecipesController < ApplicationController
     end
   end
 
+  def update
+    @recipe.update(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
   def recipe_params
     params.require(:recipe).permit(:name, :description, :image, :preparation, :user_id)
