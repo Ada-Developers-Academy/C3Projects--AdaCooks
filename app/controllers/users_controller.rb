3871c006c0e54d9effdab61d72b16ddae2ba1bcc
Corinne Pingul <corinnepingul@gmail.com>
2015-07-29 15:36:14 -0700

    Got tests for users#create to pass...thank the lord.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(valid_user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      flash.now[:errors] = ERRORS[:registration_error]
+      render :new
+    end
+  end
+
+  private
+
+  def valid_user_params
+    params.require(:user).permit(:username, :password_digest)
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,6 +38,60 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "POST #create" do
+    context "no username" do
+      before :each do
+        @user = build(:user, username: "")
+        post :create, user: attributes_for(:user, username: "")
+      end
 
+      it "doesn't persist the user to the db" do
+        expect(User.count).to eq 0
+      end
+
+      it "renders new view" do
+        expect(subject).to render_template(:new)
+      end
+
+      it "assigns flash[:errors]" do
+        expect(flash[:errors]).to include(ApplicationController::ERRORS[:registration_error])
+      end
+    end
+
+    context "duplicate username" do
+      before :each do
+        create(:user)
+        post :create, user: attributes_for(:user)
+      end
+
+      it "doesn't persist the user to the db" do
+        expect(User.where(username: "user1").count).to eq 1
+      end
+
+      it "renders new view" do
+        expect(subject).to render_template(:new)
+      end
+
+      it "assigns flash[:errors]" do
+        expect(flash[:errors]).to include(ApplicationController::ERRORS[:registration_error])
+      end
+    end
+
+    context "invalid password and password confirmation (do not match)" do
+      before :each do
+        post :create, user: attributes_for(:user, password_confirmation: "wrong")
+      end
+
+      it "doesn't persist the user to the db" do
+        expect(User.count).to eq 0
+      end
+
+      it "renders new view" do
+        expect(response).to render_template(:new)
+      end
+
+      it "assigns flash[:errors]" do
+        expect(flash[:errors]).to include(ApplicationController::ERRORS[:registration_error])
+      end
+    end
   end
 end
