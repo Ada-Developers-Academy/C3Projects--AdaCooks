18dec3ccd48431756a105c1fb49d96544a0b934b
Anita <asovay@gmail.com>
2015-07-29 13:53:43 -0700

    added samplesize constant

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   SAMPLE_SIZE = 3
 
   def index
-    @recipes = Recipe.all.sample(3)
-    @ingredients = Ingredient.all.sample(3)
+    @recipes = Recipe.all.sample(SAMPLE_SIZE)
+    @ingredients = Ingredient.all.sample(SAMPLE_SIZE)
   end
 end
