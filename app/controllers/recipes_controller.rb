ce8d6d7ec3d0f18a818c4a54984b87accf8336dd
Sally Moore <sallyamoore@gmail.com>
2015-07-29 12:55:01 -0700

    Added controller actions and view to create a new recipe. Fixed routes when I realized recipes didn't need to be nested in users. Styled the new view a bit.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,43 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def new
+    @recipe = Recipe.new
+    @ingredients = Ingredient.all
+    @cookbooks = Cookbook.where(user_id: session[:user_id])
+  end
+
+  def create
+    @recipe = Recipe.new(create_recipe_params)
+    @recipe.user = params[:user_id]
+
+    if @recipe.save
+      redirect_to root_path, notice: "Recipe added!"
+    else
+      flash.now[:error] = ERRORS[:unsuccessful_save]
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def create_recipe_params    # note: access to associated objects (e.g., cookbooks) will be through that controller.
+    params.require(:recipe).permit(
+      :name,
+      :description,
+      :preparation,
+      :image,
+    )
+  end
+
+
 end
