8a0b336ccd739d836efa43c0a531fcb9eab2151c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 17:18:49 -0700

    added note to Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,8 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :cookbooks
   has_many :ingredients, through: :recipe_ingredients
 
+  ## look up dependent destroy Active Record ramifications
+
   belongs_to :user
 
   # Validations ----------------------------------------------------------------
