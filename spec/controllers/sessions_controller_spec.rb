b4681908a42d3714fd3de2e2a51af1dd031bd9b7
Loren Wang <wang131.131@gmail.com>
2015-07-28 19:34:09 -0700

    sessions tests w/o factory girl..

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  before(:each) do
+    @user = User.create(id: 2, username: "myspacerox", password: "2002", password_confirmation: "2002")
+  end
+  after(:each) do
+    @user.destroy
+  end
+
+  describe "POST #create" do
+
+    let (:session_params) do
+            {
+              username: "myspacerox",
+              password: "2002"}
+          end
+
+    it "creates an authenticated session" do
+      post :create, :session => session_params
+      expect(session[:user_id]).to eq(2)
+      end
+  end
+
+
+  describe "DELETE #destroy" do
+    it "allows user to log out" do
+      session[:user_id] = @user.id
+      delete :destroy
+      expect(session[:user_id]).to eq(nil)
+    end
+  end
+
+  # describe "POST #create" do
+  #
+  #   let (:session_params) { create :session, username: "myspacerox", password: "2002"}
+  #
+  #   it "creates an authenticated session" do
+  #     user = build :user
+  #     session_params
+  #     post :create, :session => session_params
+  #
+  #     expect(session[:user_id]).to eq(1)
+  #     end
+  # end
+
+
+
+
+
+
+
+
+
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,5 @@
 FactoryGirl.define do
+
   factory :ingredient do
     name "Pineapple"
       # do we need a user_id???
@@ -6,6 +7,13 @@ FactoryGirl.define do
 
   factory :ingredient_params do
     # can we do this?
+  end
+
+  factory :user do
+    id 2
+    username "myspacerox"
+    password "2002"
+    password_confirmation "2002"
   end
 
 end
