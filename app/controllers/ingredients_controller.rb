7f8f5e16cb42865958e34196ba009b6b0436d2e6
Victoria <vikshab2010@hotmail.com>
2015-07-29 14:05:14 -0700

    Searching field is working, added a customized error in there is no ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,8 +8,15 @@ class IngredientsController < ApplicationController
   end
 
   def search
-     @ingredients = Ingredient.search(params[:search])
-   end
+    @ingredients = Ingredient.search(params[:search])
+    search_result = Ingredient.where(name: params[:search])
+    if !search_result.any?
+      flash[:error] = "Unfortunately we don't have #{params[:search].to_s}"
+      redirect_to root_path
+    else
+      render :search
+    end
+  end
 
   def new
     @ingredient = Ingredient.new
