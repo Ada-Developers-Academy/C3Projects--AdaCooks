b47a9f97638f5dc1950c464b142b30ee5e8f74f6
Elia <elgrenier@gmail.com>
2015-07-29 11:45:49 -0700

    before actions to check for logged in and correct users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,20 @@ class ApplicationController < ActionController::Base
 
 
   ERRORS = {
-    login_error: "Invalid login. Please try again."
+    login_error: "Invalid login. Please try again.",
+    not_logged_in: "Please log in to see this page."
   }
+  
+  # before action for new forms for which you need to be an identified user
+  def require_login
+    redirect_to login_path, flash: { errors: ERRORS[:not_logged_in] } unless session[:user_id]
+  end
+
+  # before action for being able to edit and delete recipes and ingredients
+  # or access user cookbooks
+  def correct_user
+    logged_in_user = User.find(session[:user_id])
+    redirect_to user_path(logged_in_user) unless params[:user_id] == logged_in_user.id
+  end
 
 end
