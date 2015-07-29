9f0bc32ff6b6044ea783401da705f266a560d625
amirahaile <amira.dhaile@gmail.com>
2015-07-29 10:39:06 -0700

    Fixed duplicate routes and added :new for Session.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :cookbooks, only: [:show, :edit, :destroy, :update]
   resources :recipes, :ingredients
   resources :sessions, only: [:new, :create, :destroy]
-  
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
