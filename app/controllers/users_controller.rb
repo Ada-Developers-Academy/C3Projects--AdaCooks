724c1747b15379e0cfbb814e359942d1c8abe16c
MisShellyMac <michellemccarthy@hotmail.com>
2015-07-29 14:41:46 -0700

    Initial dashboard view to show associated Recipes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @recipes = User.find(params[:id]).recipes
+    @ingredients = User.find(params[:id]).ingredients
   end
 
   def new
