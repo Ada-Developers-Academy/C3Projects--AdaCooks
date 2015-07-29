7d3d7392124f732a532ba503bf6507ae7f10d908
Kari <kari.lonczak@gmail.com>
2015-07-29 16:28:02 -0700

    Completed the 'create new recipe' function. It will associate an item with ingredients and an optional cookbook.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,22 +9,49 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+    if session[:user_id] == params[:user_id].to_i
+      @recipe = Recipe.new
 
-    @cookbooks = Cookbook.where(:user_id => params[:user_id])
+      @ingredients = Ingredient.all.order(:name)
+      @cookbooks = Cookbook.where(:user_id => params[:user_id]).order(:name)
+    else
+      flash[:error] = "You do not have access to that user's products"
+
+      redirect_to user_dashboard_path(session[:user_id])
+    end
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = Recipe.create(recipe_params)
 
     if @recipe.save
-  
+      associate_ingredients(@recipe)
+      associate_cookbooks(@recipe)
     else
       flash[:error] = "Please enter valid values"
-
     end
-      
-      redirect_to user_dashboard_path(session[:user_id])
+
+    redirect_to user_dashboard_path(session[:user_id])
+  end
+
+  def associate_ingredients(recipe)
+    recipe.ingredients.destroy_all
+
+    input_ingredients = params[:recipe][:ingredients]
+
+    input_ingredients.each do |input|
+      if input != ""
+        recipe.ingredients << Ingredient.find(input)
+      end
+    end
+  end
+
+  def associate_cookbooks(recipe)
+    cookbook_id = params[:recipe][:cookbooks].to_i
+
+    if cookbook_id != 0
+      recipe.cookbooks << Cookbook.find(cookbook_id)
+    end
 
   end
 
