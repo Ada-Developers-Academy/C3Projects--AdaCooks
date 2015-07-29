7cc1c81b03e0dba34064ab4e49d1d65acbc894e1
amirahaile <amira.dhaile@gmail.com>
2015-07-28 18:24:57 -0700

    Added and spec'd #index.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,6 @@
 class RecipesController < ApplicationController
+
+	def index
+		@recipes = Recipe.alphabetize
+	end
 end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
+	describe "GET index" do
+		before :each do
+			["E", "C", "B", "D", "F", "A"].each do |letter|
+				create :recipe, name: letter
+			end
+		end
 
+		it "lists all the recipes" do
+			get :index
+			expect(assigns(:recipes).count).to eq Recipe.count
+		end
+
+		it "lists the recipes alphabetically" do
+			get :index
+
+			title_order = []
+			assigns(:recipes).each do |recipe|
+				title_order << recipe.name
+			end
+
+			expect(title_order).to eq ["A", "B", "C", "D", "E", "F"]
+		end
+
+		it "renders the :index view" do
+			get :index
+			expect(subject).to render_template :index
+		end
+	end
 end
