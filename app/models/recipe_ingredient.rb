2a8a4d2a8eb3b765788b6edae4bea47021e99bc2
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 21:39:57 -0700

    added commented formatting

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,9 +1,10 @@
 class RecipeIngredient < ActiveRecord::Base
+  # Associations ---------------------------------------------------------------
   belongs_to :recipe
   belongs_to :ingredient
   belongs_to :measurement
 
+  # Validations ----------------------------------------------------------------
   validates :recipe_id, presence: true
   validates :ingredient_id, presence: true
-
 end
