2e975740d25646afba8a178d0c4c62158c3bafaf
amirahaile <amira.dhaile@gmail.com>
2015-07-29 10:40:44 -0700

    Fixed duplicate :recipe in factories and re-migrated.

--- db/schema.rb
+++ db/schema.rb
@@ -38,16 +38,6 @@ ActiveRecord::Schema.define(version: 20150729184448) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.string   "description", null: false
-    t.string   "image_url"
-    t.string   "prep",        null: false
-    t.integer  "cookbook_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
     t.string   "email",           null: false
