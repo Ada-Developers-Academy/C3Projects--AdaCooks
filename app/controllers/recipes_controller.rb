5a5a264df09ef652417d84f6a8f3bbd1fc7a1c69
Corinne Pingul <corinnepingul@gmail.com>
2015-07-29 07:30:30 -0700

    Added tests for recipe's show method. Added the method. Added some styling that is WIP.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,8 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.alpha_order
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -11,14 +11,33 @@ RSpec.describe RecipesController, type: :controller do
     it "instantiates a variable that is all the recipes in alpha_order" do
       get :index
 
-      expect(response.status).to eq 200
       expect(assigns(:recipes).first.name).to eq "Appetite"
     end
 
     it "renders the recipes#index view" do
       get :index
 
+      expect(response.status).to eq 200
       expect(subject).to render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    context "not logged in user" do
+      let(:recipe) { create(:recipe) }
+
+      it "renders the show page" do
+        get :show, id: recipe.id
+
+        expect(response.status).to eq 200
+        expect(subject).to render_template :show
+      end
+
+      it "instantiates a variable to store the recipe" do
+        get :show, id: recipe.id
+        
+        expect(assigns(:recipe).id).to eq 1
+      end
     end
   end
 end
