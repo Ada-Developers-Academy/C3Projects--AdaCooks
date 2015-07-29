43e47c935a87fe2ad1b7422d92118ba08f21e3a0
Jeri <drvonnjerryxlii@gmail.com>
2015-07-29 13:15:26 -0700

    added specs for #random_profession_name & #format_date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ module ApplicationHelper
   def format_date(date) # FIXME: test #format_date
     # OPTIMIZE: is this the best format?
     # http://apidock.com/ruby/DateTime/strftime
-    date.strftime("%B %Y") # eg, January 2086, August 1994
+    if (date.class == Time) || (date.class == Date) || (date.class == DateTime)
+      date.strftime("%B %Y") # eg, January 2086, August 1994
+    end
   end
 
   def random_profession_name # FIXME: test #random_profession_name
@@ -11,7 +13,7 @@ module ApplicationHelper
       "artists", "musicians", "grad students", "professors", "teachers",
       "lifeguards", "firefighters", "nurses", "architects", "video game testers",
       "programmers", "elephants", "giraffes", "pilots", "stewards",
-      "most children", "superheroes"
+      "most children", "superheroes", "librarians", "actors", "scientists" # NOTE: anything past this point is not yet in the specs
     ]
 
     professionals.shuffle.sample.capitalize  # just shuffling for extra randomness
