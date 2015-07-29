0e1ffe9199e432e6968ea666fd7518b5811090b6
Shanna Chau <shannachau@gmail.com>
2015-07-29 11:42:57 -0700

    converting params[:user_id] to int to match session[:user_id]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def require_user_login
-    unless session[:user_id] == params[:user_id]
+    unless session[:user_id] == params[:user_id].to_i
       flash[:error] = "You do not have access to this page."
       redirect_to root_path
     end
