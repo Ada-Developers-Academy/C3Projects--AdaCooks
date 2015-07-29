f04397b9e9b4e16815e2df16b10e33d38a4f7d7b
amirahaile <amira.dhaile@gmail.com>
2015-07-29 16:03:40 -0700

    Created the form to make a new recipe using NESTED FORMS. Thanks TA Jeremy 🙌🏾

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,34 @@ class RecipesController < ApplicationController
 	def index
 		@recipes = Recipe.alphabetize
 	end
+
+	def new
+		@recipe = Recipe.new
+		@recipe.recipe_ingredients.build
+
+		# move to model?
+		user_cookbooks = Cookbook.where(user_id: session[:user_id]).map { |c| [c.name, c.id] }
+		@cookbooks_array = user_cookbooks.unshift(["None", nil])
+
+		# move to model?
+		count = 1
+		@measurements_array = []
+		measurements = RecipeIngredient::MEASUREMENTS.each do |measurement|
+			@measurements_array << [measurement, count]
+			count += 1
+		end
+	end
+
+	def create
+		recipe = Recipe.create(create_params)
+		redirect_to recipe_path(recipe)
+	end
+
+	private
+
+	def create_params
+		params.require(:recipe).permit(:name, :description, :image_url, :prep,
+			:cookbook_id, recipe_ingredients_attributes: [:quantity, :measurement,
+			:ingredient_name])
+	end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,21 @@
 class RecipeIngredient < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :ingredient
+
+  # how could you make these pluralize when needed?
+  MEASUREMENTS = ["", "tsp", "tbsp", "cup", "ounce", "gram", "pound", "piece"]
+
+  # getter - for edit
+  def ingredient_name
+    # ingredient.nil? ? nil : ingredient.name
+    # doesn't need self cause it'll check for a variable first which doesn't exist
+    # then it'll check for an object
+    ingredient.try :name
+  end
+
+  # setter - for creating
+  def ingredient_name=(name)
+    # use self to assign to object
+    self.ingredient = Ingredient.where(name: name).first_or_create
+  end
 end
