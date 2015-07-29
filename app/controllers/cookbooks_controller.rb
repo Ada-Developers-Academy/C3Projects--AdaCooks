583d6960d906f985beda1601887ffc40dd18f492
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 22:25:30 -0700

    wrote create action and tests

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,13 +1,29 @@
 class CookbooksController < ApplicationController
-  # before_action :get_user, only: [:new, :create]
+  MESSAGES = {
+    cookbook_create_success: "You have successfully created a new cookbook.",
+    cookbook_create_fail: "There was a problem with your new cookbook. Please try again."
+  }
 
   def new
     @cookbook = Cookbook.new
   end
 
+  def create
+    @cookbook = Cookbook.new(cookbook_params)
+
+    if @cookbook.save
+      flash[:success] = MESSAGES[:cookbook_create_success]
+      # redirect_to cookbook_path
+      redirect_to root_path
+    else
+      flash[:errors] = MESSAGES[:cookbook_create_fail]
+      render :new
+    end
+  end
+
   private
 
-  # def get_user
-  #   @user = User.find(params[:user_id])
-  # end
+  def cookbook_params
+    params.require(:cookbook).permit(:name, :description, :user_id)
+  end
 end
--- spec/controllers/cookbooks_controller_spec.rb
+++ spec/controllers/cookbooks_controller_spec.rb
@@ -14,4 +14,36 @@ RSpec.describe CookbooksController, type: :controller do
       expect(response).to render_template("new")
     end
   end
+
+  describe "post #create" do
+    context "valid params" do
+      let(:valid_params) do
+        { name: "a name", description: "a description", user_id: 1 }
+      end
+
+      it "creates a new cookbook" do
+        post :create, :cookbook => valid_params
+        expect(Cookbook.count).to eq 1
+      end
+    # write test for redirect to cookbook#show when dashboard is created
+    end
+
+    context "invalid params" do
+      let(:invalid_params) do
+        { name: "a name", description: "a description", user_id: nil }
+      end
+
+      before :each do
+        post :create, :cookbook => invalid_params
+      end
+
+      it "does not create a new cookbook" do
+        expect(Cookbook.count).to eq 0
+      end
+
+      it "renders the new template" do
+        expect(response).to render_template("new")
+      end
+    end
+  end
 end
