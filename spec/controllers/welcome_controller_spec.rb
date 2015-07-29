4a687029772a5b8d27fa7ca3f42d8eb85d120706
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-28 19:48:53 -0700

    Added test for #index

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-
+  describe "Actions" do
+    describe "GET #index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+    end
+  end
 end
