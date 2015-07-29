95365578e85dbf24416e5ebe8c35179082a27f9b
Jeri <drvonnjerryxlii@gmail.com>
2015-07-28 22:01:32 -0700

    added columns to recipes#index & ingredients#index

--- app/helpers/ingredients_helper.rb
+++ app/helpers/ingredients_helper.rb
@@ -1,2 +1,7 @@
 module IngredientsHelper
+  def ingredient_recipes_count(ingredient) # FIXME: test ingredient_recipes_count
+    output = "(in #{ ingredient.recipes.count } recipe"
+    output += "s" unless ingredient.recipes.count == 1
+    output += ")"
+  end
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,7 @@
 module RecipesHelper
+  def recipe_ingredients_count(recipe) # FIXME: test recipe_ingredients_count
+    output = "(#{ recipe.ingredients.count } ingredient"
+    output += "s" unless recipe.ingredients.count == 1
+    output += ")"
+  end
 end
