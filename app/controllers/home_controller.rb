9ad0382363d7ec85f23fef8c539181661d3719d2
Anita <asovay@gmail.com>
2015-07-29 13:45:41 -0700

    added carrier wave requirements and showing images on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,10 @@ class HomeController < ApplicationController
   include ApplicationHelper
   before_action :define_user
 
+  SAMPLE_SIZE = 3
+
   def index
-    @recipes = Recipe.all
-    @cookbooks = Cookbook.all
-    @ingredients = Ingredient.all
+    @recipes = Recipe.all.sample(3)
+    @ingredients = Ingredient.all.sample(3)
   end
 end
