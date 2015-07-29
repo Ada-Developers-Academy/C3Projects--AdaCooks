278503d7d582ae19672767c6412a19de3e4b58a7
Loren Wang <wang131.131@gmail.com>
2015-07-28 20:26:33 -0700

    recipe controller and models wip

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,12 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :ingredients
   has_and_belongs_to_many :cookbooks
   belongs_to :user
+
+  # Validations ----------------------------------------------------------------
+  validates :name, presence: true
+  validates :preparation, presence: true
+
+  # Mounted Objects_____________________________________________________________
+  mount_uploader :image, ImageUploader #instance of class image uploader
+
 end
