059ed0a22d3bfe54a4ab43b7fcdd50bf8d00fe39
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-29 00:07:52 -0700

    added flash messages and checks to #destroy action

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,11 +1,14 @@
 class CookbooksController < ApplicationController
   before_action :set_cookbook, only: [:edit, :update, :show, :destroy]
+  before_action :current_user, only: [:edit, :update, :show, :destroy]
 
   MESSAGES = {
     create_success: "You have successfully created a new cookbook.",
     create_fail: "There was a problem with your new cookbook. Please try again.",
     update_success: "You have successfully updated your cookbook.",
-    update_fail: "There was a problem with your update. Please try again."
+    update_fail: "There was a problem with your update. Please try again.",
+    destroy_success: "You have successfully deleted the cookbook.",
+    destroy_fail: "There was a problem with your cookbook deletion. Please try again."
   }
 
   def new
@@ -42,9 +45,15 @@ class CookbooksController < ApplicationController
   end
 
   def destroy
+    cookbook_id = @cookbook.id
     @cookbook.destroy
-
-    redirect_to root_path #user_path(@current_user)
+    if Cookbook.find_by(id: cookbook_id)
+      flash[:errors] = MESSAGES[:destroy_fail]
+      redirect_to :back
+    else
+      flash[:success] = MESSAGES[:destroy_success]
+      redirect_to root_path #user_path(@current_user)
+    end
   end
 
   private
