463465926e96bab6a70408c03a62cb07fbdbbbe8
Ashley Watkins <AWatkins31@gmail.com>
2015-07-29 12:45:30 -0700

    Adds recipes#show + tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,15 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
+
   def index
     @recipes = Recipe.by_name
   end
+
+  def show
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
 end
