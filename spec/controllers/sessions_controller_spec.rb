cdc5be1357a55e6844b671bc983e0bcf09c047c1
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-29 16:23:53 -0700

    Removed commented out test.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  # FIXME: Figure out how to test this since we are not using a new view
-  # describe "GET #new" do
-  #   it "responds successfully with an HTTP 200 status code" do
-  #     get :new
-  #
-  #     expect(response).to be_success
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
-
   describe "POST #create" do
     it "without login, session[:user_id] is nil" do
       create :user
