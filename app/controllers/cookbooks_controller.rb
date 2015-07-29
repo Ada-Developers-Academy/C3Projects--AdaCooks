2295534de6b8764bbaa8d94511b97444824f9427
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 23:01:21 -0700

    added tests and actions for #edit and #update

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,7 +1,11 @@
 class CookbooksController < ApplicationController
+  before_action :set_cookbook, only: [:edit, :update]
+
   MESSAGES = {
-    cookbook_create_success: "You have successfully created a new cookbook.",
-    cookbook_create_fail: "There was a problem with your new cookbook. Please try again."
+    create_success: "You have successfully created a new cookbook.",
+    create_fail: "There was a problem with your new cookbook. Please try again.",
+    update_success: "You have successfully updated your cookbook.",
+    update_fail: "There was a problem with your update. Please try again."
   }
 
   def new
@@ -12,16 +16,36 @@ class CookbooksController < ApplicationController
     @cookbook = Cookbook.new(cookbook_params)
 
     if @cookbook.save
-      flash[:success] = MESSAGES[:cookbook_create_success]
+      flash[:success] = MESSAGES[:create_success]
       # redirect_to cookbook_path
       redirect_to root_path
     else
-      flash[:errors] = MESSAGES[:cookbook_create_fail]
+      flash[:errors] = MESSAGES[:create_fail]
       render :new
     end
   end
 
+  def edit
+    # @cookbook = Cookbook.find(params[:id])
+  end
+
+  def update
+    # @cookbook = Cookbook.find(params[:id])
+    @cookbook.update(cookbook_params)
+    if @cookbook.save
+      flash[:success] = MESSAGES[:update_success]
+      redirect_to root_path # @cookbook
+    else
+      flash[:errors] = MESSAGES[:update_fail]
+      render :edit
+    end
+  end
+
   private
+
+  def set_cookbook
+    @cookbook = Cookbook.find(params[:id])
+  end
 
   def cookbook_params
     params.require(:cookbook).permit(:name, :description, :user_id)
