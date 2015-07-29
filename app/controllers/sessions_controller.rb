29d119c6aea357fdc04818b2245d9435d91e1494
Elia <elgrenier@gmail.com>
2015-07-29 11:18:18 -0700

    passing specs for create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,22 @@
 class SessionsController < ApplicationController
+  before_action :set_user, only: [:create]
 
   def new; end
 
   def create
+    # login using username
+    if @user && @user.authenticate(params[:session][:password]) # user was found
+      session[:user_id] = @user.id # setting session to the user.id
+      redirect_to user_path(@user)
+    else # user wasn't found
+      flash.now[:errors] = ERRORS[:login_error]
+      render "new"
+    end
   end
 
+  private
+
+      def set_user
+      @user = User.find_by(username: params[:session][:username])
+      end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,38 +9,45 @@ RSpec.describe SessionsController, type: :controller do
     expect(response).to be_success
     expect(response).to have_http_status(200)
     end
+  end
 
-    describe "POST #create creates a new session" do
+  describe "POST #create creates a new session" do
+    before :each do
+      @user = create :user
+    end
+    context "invalid login" do
+      it "without login session id is nil" do
+
+        expect(session[:user_id]).to be nil
+      end
+
+      it "invalid password renders the page with error message" do
+        post :create, session: { username: @user.username, password: 'invalid' }
+
+        expect(response).to render_template(:new)
+        expect(flash[:errors]).to include { :login_error }
+      end
+
+      it "invalid username renders the page with error message" do
+        post :create, session: { username: "invalid_user", password: @user.password }
+
+        expect(response).to render_template(:new)
+        expect(flash[:errors]).to include { :login_error }
+      end
+    end
+
+    context "valid login" do
       before :each do
-        user = create :user
-      end
-      context "invalid login" do
-        it "without login session id is nil" do
-
-          expect(session[:user_id]).to be nil
-        end
-
-        it "invalid password renders the page with error message" do
-          post :create, session: { username: user.username, password: 'invalid' }
-
-          expect(response).to render_template(:new)
-          expect(flash[:errors]).to include { :login_error }
-        end
-
-        it "invalid username renders the page with error message" do
-          post :create, session: { username: "invalid_user", password: user.password }
-
-          expect(response).to render_template(:new)
-          expect(flash[:errors]).to include { :login_error }
-        end
+        post :create, session: {username: @user.username, password: @user.password}
       end
 
-      context "valid login" do
-        it "with a login session id is equal to user id" do
-          post :create, session: {username: user.username, password: user.password}
+      it "session id is equal to user id" do
+        expect(session[:user_id]).to eq @user.id
+      end
 
-          expect(session[:user_id]).to eq user.id
-        end
+      it "redirects to the user's show page" do
+        expect(response).to redirect_to user_path(@user)
+        expect(response).to have_http_status(302)
       end
     end
   end
