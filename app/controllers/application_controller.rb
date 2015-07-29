dd630e9acf8688caef6dd2d02804e32314232847
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-29 11:35:42 -0700

    Changed @user to @username.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_username
 
   # if trying to access a location as a non-logged in user
   def require_login
@@ -9,8 +10,8 @@ class ApplicationController < ActionController::Base
       flash: { warning: "You need to be a signed-in user." } unless session[:user_id]
   end
 
-  # to make an accessible user variable for the header, if logged in
-  def set_logged_in_user
-    @user = User.find(session[:user_id]) unless session[:user_id].nil?
+  # used in the views as a clickable link in the header, if logged in
+  def set_username
+    @username = User.find(session[:user_id]).username unless session[:user_id].nil?
   end
 end
