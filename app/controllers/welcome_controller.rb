63e0fd101192c61c1d589c5f0e9a963868057b40
Elsa <elsa.moluf@gmail.com>
2015-07-28 17:00:02 -0700

    Alphabetically browse recipes and ingredients on home index.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
   def index
     if session[:user_id]
       @user = User.find_by(id: session[:user_id])
+
+      @recipes = Recipe.order(:name)
+      @ingredients = Ingredient.order(:name)
     end
   end
 end
