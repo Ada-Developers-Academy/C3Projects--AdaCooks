57fb40e53695b231f194f89b4d1c99539b1f7880
Kari <kari.lonczak@gmail.com>
2015-07-29 15:17:34 -0700

    Started working on user dashboard including the ability to add a new recipe.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,32 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+
+    @cookbooks = Cookbook.where(:user_id => params[:user_id])
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+  
+    else
+      flash[:error] = "Please enter valid values"
+
+    end
+      
+      redirect_to user_dashboard_path(session[:user_id])
+
+  end
+
+###########################################################
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation, :image, :categories, :user_id)
+  end
+
 end
