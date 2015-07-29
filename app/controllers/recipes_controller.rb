8ee8c8b1246701da2eb223091436b5aa2ff50273
Loren Wang <wang131.131@gmail.com>
2015-07-28 18:34:15 -0700

    users dashboard wip

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,26 @@
 class RecipesController < ApplicationController
   def index
-  @recipes = Recipe.all
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    @recipe.user_id = session[:user_id]
+
+    if @recipe.save
+        redirect_to user_path(session[:user_id])
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :image, {:cookbook_ids => [] })
   end
 end
