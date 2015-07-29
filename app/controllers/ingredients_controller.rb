d624c918b484cecbb7e2145d568df029d9784134
MisShellyMac <michellemccarthy@hotmail.com>
2015-07-29 16:53:29 -0700

    Added ability to visit dashboard via btn after login, added ability to edit/destroy an ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -9,6 +9,38 @@ class IngredientsController < ApplicationController
     @recipes = Ingredient.find(params[:id]).recipes
   end
 
+  def new
+    @ingredient = Ingredient.new
+  end
 
+  def create
+    @ingredient = Ingredient.create(create_params[:ingredient])
+    @ingredient.user_id = session[:user_id]
+    @ingredient.save
+    redirect_to user_dashboard_path(session[:user_id])
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(create_params[:ingredient])
+    redirect_to user_dashboard_path(session[:user_id])
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+    redirect_to user_dashboard_path(session[:user_id])
+  end
 
 end
+
+###########################################################
+  private
+
+  def create_params
+    params.permit(ingredient: [:name, :image ,:user_id, :recipe_id])
+  end
