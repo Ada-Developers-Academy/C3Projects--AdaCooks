ee1eca72f88d333e2dfb72eb7b53d6148ae03161
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-29 15:54:40 -0700

    Added show action and show view.

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,2 +1,8 @@
 class CookbooksController < ApplicationController
+  before_action :require_login, only: [:show]
+
+  def show
+    @cookbook = Cookbook.find(params[:id])
+    @recipes = @cookbook.recipes
+  end
 end
