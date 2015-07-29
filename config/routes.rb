3b7883faf4369132b76f4b5b78d50a94c9f8896f
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-28 23:11:28 -0700

    added cookbooks#show route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy', as: 'signout'
 
-  resources :cookbooks, only: [:new, :create, :edit, :update]
+  resources :cookbooks, only: [:new, :create, :edit, :update, :show]
 end
