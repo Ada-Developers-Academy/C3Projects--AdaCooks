0ef824103c6d8aa9d1359e769e0948880e9c8535
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-29 09:00:04 -0700

    working on remove_recipe function; WIP

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,6 +1,6 @@
 class CookbooksController < ApplicationController
-  before_action :set_cookbook, only: [:edit, :update, :show, :destroy]
-  before_action :current_user, only: [:edit, :update, :show, :destroy]
+  before_action :set_cookbook, only: [:edit, :update, :show, :destroy, :remove_recipe]
+  before_action :current_user, only: [:edit, :update, :show, :destroy, :remove_recipe]
 
   MESSAGES = {
     create_success: "You have successfully created a new cookbook.",
@@ -54,6 +54,20 @@ class CookbooksController < ApplicationController
       flash[:success] = MESSAGES[:destroy_success]
       redirect_to root_path #user_path(@current_user)
     end
+  end
+
+  def remove_recipe
+    # cookbook = Cookbook.find(params[:id])
+    recipe = @cookbook.recipes.find(params[:id])
+    raise
+    if recipe && @cookbook
+      @cookbook.recipes.delete(recipe)
+      recipe.cookbooks.delete(@cookbook)
+      flash[:success] = MESSAGES[:remove_success]
+    else
+      flash[:error] = MESSAGES[:remove_fail]
+    end
+      redirect_to user_cookbook_path(@user, @cookbook)
   end
 
   private
