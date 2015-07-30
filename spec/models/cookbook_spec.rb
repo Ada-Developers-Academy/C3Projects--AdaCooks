0a017b55b2c5e4bc223c3e27c40dee7fe0c96fde
Loren Wang <wang131.131@gmail.com>
2015-07-30 00:18:54 -0700

    moar cookbook model tests

--- spec/models/cookbook_spec.rb
+++ spec/models/cookbook_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Cookbook, type: :model do
+  # + case
+  it "creates cookbook if name is present" do
+    cookbook1 = build :cookbook
+    expect(cookbook1).to be_valid
+  end
+  # - case
   it "Requires that a name must be present" do
     cookbook1 = build :cookbook, name: nil
 
