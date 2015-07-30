27135ba3250c97fa6a8ffb796e661a6085d2f499
Jeri <drvonnjerryxlii@gmail.com>
2015-07-30 14:28:32 -0700

    wip ingredients controller specs

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -38,4 +38,299 @@ RSpec.describe IngredientsController, type: :controller do
       expect(assigns(:ingredient)).to eq(ingredient)
     end
   end
+
+  describe "GET #new" do
+    before :each do
+      @user = create :user
+      # @ingredient = create :ingredient, user_id: @user.id
+      session[:user_id] = @user.id
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :new, user_id: @user.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders new template" do
+      get :new, user_id: @user.id
+      expect(response).to render_template("new")
+    end
+
+    # it "assigns @ingredient" do # FIXME: Ingredient.new??
+    #   get :new, user_id: @user.id
+    #   expect(assigns(:ingredient)).to eq(@ingredient)
+    # end
+
+    it "assigns @authenticated_user" do
+      get :new, user_id: @user.id
+      expect(assigns(:authenticated_user)).to eq(@user)
+    end
+
+    context "unauthenticated users" do
+      before :each do
+        session[:user_id] = nil
+      end
+
+      it "does not permit access / redirects to the home page" do
+        get :new, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "authenticated users that don't own current ingredient" do
+      before :each do
+        @user2 = create :user, username: "another user", email: "another@use.r"
+        session[:user_id] = @user2.id
+      end
+
+      it "does not permit access / redirects to the home page" do
+        get :new, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "GET #edit" do
+    before :each do
+      @user = create :user
+      @ingredient = create :ingredient, user_id: @user.id
+      session[:user_id] = @user.id
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :edit, id: @ingredient.id, user_id: @user.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders edit template" do
+      get :edit, id: @ingredient.id, user_id: @user.id
+      expect(response).to render_template("edit")
+    end
+
+    it "assigns @ingredient" do
+      get :edit, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:ingredient)).to eq(@ingredient)
+    end
+
+    it "assigns @authenticated_user" do
+      get :edit, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:authenticated_user)).to eq(@user)
+    end
+
+    context "unauthenticated users" do
+      before :each do
+        session[:user_id] = nil
+      end
+
+      it "does not permit access / redirects to the home page" do
+        get :edit, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "authenticated users that don't own current ingredient" do
+      before :each do
+        @user2 = create :user, username: "another user", email: "another@use.r"
+        session[:user_id] = @user2.id
+      end
+
+      it "does not permit access / redirects to the home page" do
+        get :edit, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    before :each do
+      @user = create :user
+      @ingredient = build :ingredient, user_id: nil
+      session[:user_id] = @user.id
+    end
+
+    it "assigns @ingredient" do
+      post :create, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:ingredient)).to eq(@ingredient)
+    end
+
+    it "assigns @authenticated_user" do
+      post :create, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:authenticated_user)).to eq(@user)
+    end
+
+    context "valid form input" do
+      it "redirects to newly created ingredient's show page" do
+        post :create, id: @ingredient.id, user_id: @user.id
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(ingredient_path(@ingredient))
+      end
+
+      it "renders edit template" do
+        post :create, id: @ingredient.id, user_id: @user.id
+        expect(response).to render_template("edit")
+      end
+    end
+
+    context "invalid form input" do
+      it "responds successfully with an HTTP 200 status code" do
+        post :create, id: @ingredient.id, user_id: @user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders new template" do
+        post :create, id: @ingredient.id, user_id: @user.id
+        expect(response).to render_template("new")
+      end
+    end
+
+    context "unauthenticated users" do
+      before :each do
+        session[:user_id] = nil
+      end
+
+      it "does not permit access / redirects to the home page" do
+        post :create, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "authenticated users that don't own current ingredient" do
+      before :each do
+        @user2 = create :user, username: "another user", email: "another@use.r"
+        session[:user_id] = @user2.id
+      end
+
+      it "does not permit access / redirects to the home page" do
+        post :create, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    before :each do
+      @user = create :user
+      @ingredient = create :ingredient, user_id: @user.id
+      session[:user_id] = @user.id
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      patch :update, id: @ingredient.id, user_id: @user.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders edit template" do
+      patch :update, id: @ingredient.id, user_id: @user.id
+      expect(response).to render_template("edit")
+    end
+
+    it "assigns @ingredient" do
+      patch :update, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:ingredient)).to eq(@ingredient)
+    end
+
+    it "assigns @authenticated_user" do
+      patch :update, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:authenticated_user)).to eq(@user)
+    end
+
+    context "unauthenticated users" do
+      before :each do
+        session[:user_id] = nil
+      end
+
+      it "does not permit access / redirects to the home page" do
+        patch :update, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "authenticated users that don't own current ingredient" do
+      before :each do
+        @user2 = create :user, username: "another user", email: "another@use.r"
+        session[:user_id] = @user2.id
+      end
+
+      it "does not permit access / redirects to the home page" do
+        patch :update, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before :each do
+      @user = create :user
+      @ingredient = create :ingredient, user_id: @user.id
+      session[:user_id] = @user.id
+    end
+
+    it "redirects to ingredients#index" do
+      delete :destroy, id: @ingredient.id, user_id: @user.id
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(ingredients_path)
+    end
+
+    it "assigns @ingredient" do
+      delete :destroy, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:ingredient)).to eq(@ingredient)
+    end
+
+    it "assigns @authenticated_user" do
+      delete :destroy, id: @ingredient.id, user_id: @user.id
+      expect(assigns(:authenticated_user)).to eq(@user)
+    end
+
+    it "DESTROYS the ingredient" do
+      delete :destroy, id: @ingredient.id, user_id: @user.id
+      expect(Ingredient.find_by(id: @ingredient.id)).to eq(nil)
+    end
+
+    context "unauthenticated users" do
+      before :each do
+        session[:user_id] = nil
+      end
+
+      it "does not permit access / redirects to the home page" do
+        delete :destroy, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "authenticated users that don't own current ingredient" do
+      before :each do
+        @user2 = create :user, username: "another user", email: "another@use.r"
+        session[:user_id] = @user2.id
+      end
+
+      it "does not permit access / redirects to the home page" do
+        delete :destroy, id: @ingredient.id, user_id: @user.id
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 end
