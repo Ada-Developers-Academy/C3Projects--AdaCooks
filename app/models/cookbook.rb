bb342a243c5c44c66a10fda5da89a786090cd8e5
Kari <kari.lonczak@gmail.com>
2015-07-30 15:38:33 -0700

    Created specs for cookbook model.

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -6,5 +6,4 @@ class Cookbook < ActiveRecord::Base
   has_and_belongs_to_many :recipes
   has_many :ingredients, :through => :recipes
   belongs_to :user
-
 end
--- spec/models/cookbook_spec.rb
+++ spec/models/cookbook_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Cookbook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "model validations" do
+    it "has and belongs to many recipes" do
+      expect(Cookbook.reflect_on_association(:recipes).macro).to eq(:has_and_belongs_to_many)
+    end
+
+    it "has_many ingredients" do
+      expect(Cookbook.reflect_on_association(:ingredients).macro).to eq(:has_many)
+    end
+
+    it "belongs_to user" do
+      expect(Cookbook.reflect_on_association(:user).macro).to eq(:belongs_to)
+    end
+  end
 end
