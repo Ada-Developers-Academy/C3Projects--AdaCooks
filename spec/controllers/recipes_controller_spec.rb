ba5c5d58bb843b1f52cd66b485aa462e8f0de226
amirahaile <amira.dhaile@gmail.com>
2015-07-30 09:27:18 -0700

    Spec'd out the recipes controller #new and #create.

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -31,9 +31,32 @@ RSpec.describe RecipesController, type: :controller do
 	end
 
 	describe "GET new" do
+		before :each do
+			["A", "B", "C"].each do |letter|
+				create :cookbook, name: letter
+			end
+
+			create :user
+			session[:user_id] = 1
+		end
+
 		it 'assigns an empty Recipe object' do
 			get :new
 			expect(assigns(:recipe)).to be_a_kind_of Recipe
+		end
+
+		it 'makes an array of cookbooks names and their IDs' do
+			expected_array = [["None", nil], ["A", 1], ["B", 2], ["C", 3]]
+			get :new
+
+			expect(assigns(:cookbooks_array)).to eq expected_array
+		end
+
+		it 'makes an array of measurment values' do
+			expected_array = [["", ""], ["tsp", "tsp"], ["tbsp", "tbsp"], ["cup", "cup"], ["ounce", "ounce"], ["gram", "gram"], ["pound", "pound"], ["piece", "piece"]]
+			get :new
+
+			expect(assigns(:measurements_array)).to eq expected_array
 		end
 	end
 
@@ -68,7 +91,7 @@ RSpec.describe RecipesController, type: :controller do
 		end
 
 		context "object did not save" do
-			let(:params) { { :recipe => { name: "recipe", description: "it's good",
+			let(:params) { { :recipe => { description: "it's good",
 				prep: "mix well", recipe_ingredients_attributes: [quantity: 1, measurement: "cup",
 				ingredient_name: "lemon"] } } }
 
