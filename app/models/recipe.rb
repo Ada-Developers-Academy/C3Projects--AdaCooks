5e8b1eed6bf78c959d28777280338e191e741d50
Loraine <loraine@lorainekv.com>
2015-07-30 09:47:51 -0700

    Added an accepts_nested_attributes association.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,9 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   belongs_to :user
+  accepts_nested_attributes_for :recipe_ingredients,
+    allow_destroy: true,
+    reject_if: :all_blank
 
   # Validations ----------------------------------------------------------------
   validates :name, presence: true, uniqueness: true
