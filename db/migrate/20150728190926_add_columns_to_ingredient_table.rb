703fa30bf070061d48a65dca7eb221162aed6280
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-30 09:05:47 -0700

    Adds image uploader to ingredients.

--- db/migrate/20150728190926_add_columns_to_ingredient_table.rb
+++ db/migrate/20150728190926_add_columns_to_ingredient_table.rb
@@ -1,6 +1,5 @@
 class AddColumnsToIngredientTable < ActiveRecord::Migration
   def change
     add_column :ingredients, :name, :string
-    add_column :ingredients, :image, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730135259) do
+ActiveRecord::Schema.define(version: 20150730155559) do
 
   create_table "cookbooks", force: :cascade do |t|
     t.string   "name"
