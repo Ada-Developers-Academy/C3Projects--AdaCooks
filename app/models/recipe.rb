67a69435b4fedce7e050a9d83a17a7977188279c
Jeri <drvonnjerryxlii@gmail.com>
2015-07-30 13:23:06 -0700

    beginning to experiment with nested attributes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ActiveRecord::Base
   # Associations ---------------------------------------------------------------
   belongs_to :user
   has_and_belongs_to_many :ingredients
+    # accepts_nested_attributes_for :ingredients # FIXME: test accepts_nested_attributes_for :ingredients
   has_and_belongs_to_many :cookbooks
 
   # Validations ----------------------------------------------------------------
