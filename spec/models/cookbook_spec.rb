eac4a11ee60ef63ffc361537405c8af7b2fe7d65
Loren Wang <wang131.131@gmail.com>
2015-07-30 00:16:04 -0700

    working cookbook model spec

--- spec/models/cookbook_spec.rb
+++ spec/models/cookbook_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Cookbook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Requires that a name must be present" do
+    cookbook1 = build :cookbook, name: nil
+
+    expect(cookbook1).to_not be_valid
+    expect(cookbook1.errors.keys).to include(:name)
+  end
 end
