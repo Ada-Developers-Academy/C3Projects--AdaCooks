e5bb10ab202cf8e8d00bd8699539670efa1ea33f
amirahaile <amira.dhaile@gmail.com>
2015-07-30 15:11:33 -0700

    Added edit form for recipe; fixed bug of duplicating RecipeIngredients.

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,14 +3,19 @@ class RecipeIngredient < ActiveRecord::Base
   belongs_to :ingredient
 
   # how could you make these pluralize when needed?
-  MEASUREMENTS = ["", "tsp", "tbsp", "cup", "ounce", "gram", "pound", "piece"]
+  MEASUREMENTS = ["tsp", "tbsp", "cup", "ounce", "gram", "pound", "piece"]
 
   # getter - for edit
   def ingredient_name
     # doesn't need self cause it'll check for a variable first which doesn't exist
     # then it'll check for an object
-    ingredient.try :name
     # same as `ingredient.nil? ? nil : ingredient.name`
+    ingredient.try :name
+
+    # if you find the ingredient
+    # don't create a new RecipeIngredient
+    # find the RecipeIngredient with the recipe_id and the ingredient_id
+    # and return that instead
   end
 
   # setter - for creating
