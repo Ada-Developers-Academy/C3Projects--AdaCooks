34540735f198d7edf8e61b59e68ed60944af6ffe
Jeri <drvonnjerryxlii@gmail.com>
2015-07-30 10:03:28 -0700

    added #proper_name method to display name in Title Case & removed old comments

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -11,22 +11,10 @@ class Ingredient < ActiveRecord::Base
   scope :alphabetized, -> { order("name ASC") }
 
   # Mounted Objects ------------------------------------------------------------
-  mount_uploader :avatar, AvatarUploader # FIXME: we didn't do this right?
+  mount_uploader :avatar, AvatarUploader
 
-
-  # uploader = AvatarUploader.new
-  # uploader.store!(my_file)
-  # uploader.retrieve_from_store!('my_file.png')
-  # u = User.new
-  # FIRST WAY TO GET FILE
-  # u.avatar = params[:file]
-  # SECOND WAY TO GET FILE
-  # File.open('somewhere') do |f|
-  #   u.avatar = |f|
-  # end
-  # NOW YOU HAVE FILE, SO SAVE
-  # u.save!
-  # u.avatar.url # => '/url/to/file.png'
-  # u.avatar.current_path # 'path/to/file.png'
-  # u.avatar_indentifier # 'file.png'
+  # Instance Methods -----------------------------------------------------------
+  def proper_name # FIXME: test Ingredient#proper_name
+    name.titlecase
+  end
 end
