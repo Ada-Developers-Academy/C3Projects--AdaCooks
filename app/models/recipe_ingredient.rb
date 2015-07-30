7387c18354d3c046b0a1319fea4c5ffd3da6ef1e
Loraine <loraine@lorainekv.com>
2015-07-30 15:23:58 -0700

    Added an inverse_of association.

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,10 +1,10 @@
 class RecipeIngredient < ActiveRecord::Base
   # Associations ---------------------------------------------------------------
-  belongs_to :recipe
+  belongs_to :recipe, inverse_of: :recipe_ingredients
   belongs_to :ingredient
   belongs_to :measurement
 
   # Validations ----------------------------------------------------------------
-  validates :recipe_id, presence: true
+  validates :recipe, presence: true
   validates :ingredient_id, presence: true
 end
