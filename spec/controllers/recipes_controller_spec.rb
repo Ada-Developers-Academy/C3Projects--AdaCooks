f82bc51dcc1077c199eba0b39444ddc5c03a01b6
Loren Wang <wang131.131@gmail.com>
2015-07-30 15:31:45 -0700

    recipe controller specs wip

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -3,30 +3,49 @@ require 'rails_helper'
 RSpec.describe RecipesController, type: :controller do
 
   describe "GET #index" do
-    let(:recipe2) { create :recipe, name: "Cream" }
+    let(:recipe_params) do
+      {
+        recipe: {
+          name: "Yay",
+          preparation: "Yup",
+          ingredient_ids: 2,
+          user_id: 1
+        }
+      }
+    end
 
     it "renders the index template" do
-      get :index
+      get :index, recipe_params
       expect(response).to render_template("index")
     end
 
-    it "loads all recipes into @recipes" do
-      recipe = build :recipe
-      all_recipes = [recipe, recipe2]
-      get :index
-      expect(all_recipes.count).to eq 2
-    end
+    # it "loads all recipes into @recipes" do
+    #   recipe = build :recipe
+    #   all_recipes = [recipe, recipe2]
+    #   get :index
+    #   expect(all_recipes.count).to eq 2
+    # end
   end
 
   describe "GET #show" do
-  let(:soup) {create :recipe, user: create(:user) } # associated a user with a recipe: there's a user hash, create method has user paramater
+    let(:recipe_params) do
+      {
+        recipe: {
+          name: "Yay",
+          preparation: "Yup",
+          ingredient_ids: 2,
+          user_id: 1
+        }
+      }
+    end
+  # let(:soup) {create :recipe, user: create(:user) } # associated a user with a recipe: there's a user hash, create method has user paramater
     it "renders the :show view" do
-      get :show, :id => soup.id
+      get :show, recipe_params
       expect(response).to render_template("show")
     end
 
     it "returns successfully with HTTP code of 200" do
-      get :show, :id => soup.id
+      get :show, recipe_params
       expect(response).to be_success
     end
   end
@@ -78,22 +97,27 @@ RSpec.describe RecipesController, type: :controller do
         expect(response).to redirect_to(user_path(session[:user_id]))
       end
     end
+  end
 
-  #
-  # describe "DELETE #destroy" do
-  #   before(:each) do
-  #     @pine = create :ingredient
-  #     @pop = create :ingredient, name: "PopTarts"
-  #   end
-  #
-  #   it "ingredient count changes by -1" do
-  #     expect{delete :destroy, {id: @pine.id}}.to change(Ingredient, :count).by(-1)
-  #   end
-  #
-  #   it "redirect_to root_path" do
-  #     delete :destroy, id: @pine.id
-  #     expect(response).to redirect_to(root_path)
-  #   end
-   # destroy
- end
+
+  describe "DELETE #destroy" do
+    let(:recipe_params) do
+      {
+        recipe: {
+          name: "Yay",
+          preparation: "Yup",
+          ingredient_ids: 2,
+          user_id: 1
+        }
+      }
+    end
+
+    it "ingredient count changes by -1" do
+      session[:user_id] = create(:user)
+      delete :destroy, recipe_params
+      expect(Recipe).to change(Recipe, :count).by(-1)
+    end
+
+  end
+
 end
