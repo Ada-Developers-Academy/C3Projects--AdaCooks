24b6d2e449fc3fced7eca62f897cd75619028a8f
Sally Moore <sallyamoore@gmail.com>
2015-07-30 10:36:42 -0700

    WIP started ingredients new/create and tests.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -7,4 +7,43 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.find(params[:id])
     @recipes = @ingredient.recipes
   end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredeitn.new(create_ingredient_params)
+    @ingredient.user_id = session[:user_id]
+
+    if @ingredient.save
+      redirect_to ingredient_path(@ingredient.id), notice: "Ingredient added!"
+    else
+      flash.now[:errors] = ERRORS[:unsuccessful_save]
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def create_ingredient_params    # note: access to associated objects (e.g., cookbooks) will be through that controller.
+    params.require(:ingredient).permit(
+      :name,
+      :image,
+      recipe_ids: [],
+    )
+  end
+
+  def find_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
 end
--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -32,4 +32,85 @@ RSpec.describe IngredientsController, type: :controller do
       expect(response).to render_template("show")
     end
   end
+
+  ### NEW - for TDD
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # describe "POST #create" do
+  #   context "valid ingredient params" do
+  #     before :each do
+  #       create :user
+  #       session[:user_id] = 1
+  #       post :create, ingredient: attributes_for(:ingredient)
+  #     end
+  #
+  #     it "creates an Ingredient" do
+  #       expect(Ingredient.count).to eq 1
+  #     end
+  #
+  #     it "redirects to the show page" do
+  #       expect(subject).to redirect_to ingredient_path(Ingredient.first.id)
+  #     end
+  #   end
+  #
+  #   context "invalid ingredient params" do
+  #     before :each do
+  #       create :user
+  #       session[:user_id]
+  #       post :create, ingredient: attributes_for(:ingredient, name: nil)
+  #     end
+  #
+  #     it "does not create a ingredient" do
+  #       expect(Ingredient.count).to eq 0
+  #     end
+  #
+  #     # NOT WORKING AND NO ONE KNOWS WHY
+  #     # it "renders the new view" do
+  #     #   expect(response).to render_template :new
+  #     # end
+    end
+  end
+
+  describe "PUT update/:id" do
+
+    before :each do
+      @ingredient = create :ingredient
+      user = create :user
+      session[:user_id] = 1
+
+      put :update, user_id: user.id, id: 1, :ingredient => { name: "New Name"}
+      @ingredient.reload
+    end
+
+    it "updates the ingredient record" do
+      expect(response).to redirect_to(@ingredient)
+
+      expect(@ingredient.name).to eq("New Name")
+    end
+
+    it "redirects to the ingredient show page" do
+      expect(subject).to redirect_to ingredient_path(@ingredient)
+    end
+  end
+
+  describe "DELETE #destroy" do
+
+    before :each do
+      @ingredient = create :ingredient
+      create :user
+      session[:user_id] = 1
+    end
+
+    it "deletes the record" do
+      expect{
+        delete :destroy, id: @ingredient.id
+      }.to change(Ingredient, :count).by(-1)
+    end
+  end
+
 end
