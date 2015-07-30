88fa06056caa386e5412c1db4c1303d2a62c110d
Shanna Chau <shannachau@gmail.com>
2015-07-30 14:27:25 -0700

    Created helper methods to display number of recipes and unique ingredients in each cookbook

--- app/helpers/cookbooks_helper.rb
+++ app/helpers/cookbooks_helper.rb
@@ -1,2 +1,22 @@
 module CookbooksHelper
+  def cookbook_recipes_count(cookbook) # FIXME: test probably broken?
+    output = "in #{ cookbook.recipes.count } recipe"
+    output += "s" unless cookbook.recipes.count == 1
+    output += ""
+  end
+
+  def cookbook_ingredients_count(cookbook) # FIXME: test probably broken?
+    recipes = cookbook.recipes
+
+    all_ingredients = []
+    recipes.each do |recipe|
+      all_ingredients.push(recipe.ingredients)
+    end
+
+    ingredient_count = all_ingredients.flatten.uniq.count
+
+    output = "#{ ingredient_count} unique ingredient"
+    output += "s" unless ingredient_count == 1
+    output += ""
+  end
 end
