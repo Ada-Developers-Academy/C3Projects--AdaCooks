6a329312c7b2ac2df39be682c4e99f771228ecc2
Kari <kari.lonczak@gmail.com>
2015-07-30 15:47:44 -0700

    Added specs for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    it "has_many cookbooks" do
+      expect(User.reflect_on_association(:cookbooks).macro).to eq(:has_many)
+    end
+
+    it "has_many recipes" do
+      expect(User.reflect_on_association(:recipes).macro).to eq(:has_many)
+    end
+
+    it "has_many ingredients" do
+      expect(User.reflect_on_association(:ingredients).macro).to eq(:has_many)
+    end
+  end
 end
+
