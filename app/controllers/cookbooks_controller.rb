7721a20da550314acde2ff328853d9ea8a5efd62
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-29 18:27:50 -0700

    Added conditional to restrict access to only that logged in user's cookbooks.

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -2,8 +2,15 @@ class CookbooksController < ApplicationController
   before_action :require_login, only: [:show]
 
   def show
-    @cookbook = Cookbook.find(params[:id])
-    @recipes = @cookbook.recipes
-    @recipe_count = @recipes.count
+    if session[:user_id] == params[:user_id].to_i
+      @cookbook = Cookbook.find(params[:id])
+      @recipes = @cookbook.recipes
+      @recipe_count = @recipes.count
+      render :show
+    else
+      redirect_to root_path
+    end
   end
+
+  def edit; end
 end
