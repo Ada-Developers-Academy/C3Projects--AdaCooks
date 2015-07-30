ddd034845466c363a452c2caa61e4fa0161d8b7e
Ashley Watkins <AWatkins31@gmail.com>
2015-07-30 10:18:52 -0700

    Adds #by_name to the ingredient model.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "scopes" do
+    describe ".by_name" do
+      let(:ingredient_a) { create(:ingredient, name: "a") }
+      let(:ingredient_b) { create(:ingredient, name: "b") }
+      let(:ingredient_c) { create(:ingredient, name: "c") }
+
+      it "returns all ingredients in alphabetical order" do
+        ingredient_b # need to create the records in the db out of alphabetical order
+        ingredient_order = [ ingredient_a, ingredient_b, ingredient_c ]
+
+        expect(Ingredient.by_name).to eq(ingredient_order)
+      end
+    end
+  end
 end
