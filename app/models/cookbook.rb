21485664781c81e30e215b8e811238a791ed462a
Ashley Watkins <AWatkins31@gmail.com>
2015-07-30 22:18:15 -0700

    Standardized appearance across the site.
    We still can make a lot more changes! I'm not attached to this style at all.

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -1,7 +1,7 @@
 class Cookbook < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :recipes
-  has_many :ingredients, through: :recipes
+  has_many :ingredients, -> { uniq }, through: :recipes # TODO: add test for uniq
 
   # VALIDATIONS ----------------------------------------------------------------
   validates :name, presence: true
