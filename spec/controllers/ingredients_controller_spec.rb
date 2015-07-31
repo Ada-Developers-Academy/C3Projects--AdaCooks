163dcb5e19b029d8ae75a0371d361456daef83af
Elsa <elsa.moluf@gmail.com>
2015-07-31 11:22:19 -0700

    IngredientsController new test.

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe IngredientsController, type: :controller do
 
+  # def new
+  #   @ingredient = Ingredient.new
+  # end
+
+  describe "GET #new" do
+    it "renders :new view" do
+      user = create :user
+      session[:user_id] = user.id
+
+      get :new, user_id: user.id
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:new)
+    end
+  end
+
+  # describe "GET #new" do
+  #   it "renders new view" do
+  #     user = create :user
+  #     session[:user_id] = user.id
+  #     get :new, user_id: user.id
+  #
+  #     expect(response).to be_success
+  #     expect(response).to have_http_status(200)
+  #     expect(response).to render_template(:new)
+  #   end
+  # end
+
 end
