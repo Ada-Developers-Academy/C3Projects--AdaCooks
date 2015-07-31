895bb606e82602b2c5948b1dcd6527ecf80172ac
Loren Wang <wang131.131@gmail.com>
2015-07-31 09:02:20 -0700

    user controller specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login, only: [:show]
 
-
-  def index
-
-  end
-
   def new
     @user = User.new
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  
+  describe "GET #new" do
+    let(:user) {create :user}
+
+    it "creates a new ingredient" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "GET #show" do
+      before :each do
+        @user = create :user
+      end
+
+      it "returns http success" do
+        session[:user_id] = @user.id
+        get :show, :id => 1
+        expect(response).to have_http_status(:success)
+      end
+
+      it "loads a user into @user" do
+        session[:user_id] = @user.id
+        get :show, :id => session[:user_id]
+
+        expect(assigns(:user)).to eq(@user)
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid user params" do
+
+        let (:user_params) do
+          {
+            username: "beaverz",
+            password: "213",
+            password_confirmation: "213"}
+        end
+
+        it "creates a new user" do
+          post :create, :user => user_params
+          expect(User.count).to eq(1)
+        end
+      end
+
+      context "invalid user params" do
+        let (:bad_user) do
+          {
+            username: "Eeeediot",
+            password: "youu",
+            password_confirmation: "song"}
+        end
+
+        it "doesn't create a new user" do
+          post :create, :user => bad_user
+          expect(response).to redirect_to(signup_path)
+        end
+      end
+    end
 
 end
