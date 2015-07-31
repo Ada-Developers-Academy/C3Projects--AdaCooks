04e81c71b63de13eb6ed4d878d406ef893a2e0cb
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-31 14:06:34 -0700

    Removed comment.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path # TODO: change this path
+      redirect_to root_path 
     else
       flash.now[:errors] = ERRORS[:unsuccessful_signup]
       render :new
