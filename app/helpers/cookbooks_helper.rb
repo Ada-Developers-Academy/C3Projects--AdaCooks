4337cca1dad370d659416cc2c7da6911a2ebfd5d
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-31 11:41:45 -0700

    WIP cookbook show add cookbook button + authentication helper.

--- app/helpers/cookbooks_helper.rb
+++ app/helpers/cookbooks_helper.rb
@@ -1,2 +1,5 @@
 module CookbooksHelper
+  def authenticated?
+    session[:user_id] == @cookbook.user_id
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def authenticated?
+    session[:user_id] == @user.id
+  end
 end
