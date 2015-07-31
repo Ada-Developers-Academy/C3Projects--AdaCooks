2ac45a7ce6d334f5e3f560d12f94d41d789a424d
Elsa <elsa.moluf@gmail.com>
2015-07-30 23:23:25 -0700

    Minor test fiddling.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
 
+  # # def check_search
+  # #   if params[:search]
+  # #       @search_results = Recipe.joins(:ingredients).where("ingredients.name LIKE ?", "%#{params[:search][:query]}%")
+  # #   end
+  # # end
+  #
   # describe "searching for recipes by ingredients" do
   #
   #   it "returns related recipes" do
-  #     chicken = create :ingredient, id: 1, name: "chicken"
-  #     create :recipe, name: "chicken surprise", ingredient_ids: [1]
+  #     ingredient = create :ingredient
+  #     create :recipe, ingredient_ids: [ingredient.id]
   #
-  #     get :check_search, search: { query: "chcken" }
+  #     get :check_search, search: { query: "chicken" }
   #
+  #     expect(response).to be_success
+  #     expect(response).to have_http_status(200)
+  #
+  #     # don't know how to tell it it's not supposed to render a :check_search view
   #   end
   #
   # end
