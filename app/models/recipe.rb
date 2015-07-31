597d3cc003394c44773d35673f72de1c1cbe8c15
MisShellyMac <michellemccarthy@hotmail.com>
2015-07-31 13:39:37 -0700

    added validation to recipe model. rec must incl 1 ingred

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ActiveRecord::Base
   # Validations
    validates :name, presence: true
+   validates :ingredients, :length => { :minimum => 1 }
   # validates :preparation, presence: true
   # validates :ingredients, presence: true
 
