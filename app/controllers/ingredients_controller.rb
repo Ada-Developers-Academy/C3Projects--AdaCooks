230c90d554f45ada076727e4e052b2091fcd1271
Kari <kari.lonczak@gmail.com>
2015-07-31 13:39:36 -0700

    Cannot delete an ingredient if it is the last ingredient in an existing recipe.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -24,6 +24,7 @@ class IngredientsController < ApplicationController
   def create
     @ingredient = Ingredient.create(create_params[:ingredient])
     @ingredient.user_id = session[:user_id]
+
     @ingredient.save
     redirect_to user_dashboard_path(session[:user_id])
   end
@@ -40,7 +41,21 @@ class IngredientsController < ApplicationController
 
   def destroy
     @ingredient = Ingredient.find(params[:id])
-    @ingredient.destroy
+
+    single_ingredient_recipes = []
+
+    @ingredient.recipes.each do |recipe|
+      if recipe.ingredients.count == 1
+        single_ingredient_recipes.push(recipe)
+      end
+    end
+
+    if single_ingredient_recipes.length == 0
+      @ingredient.destroy
+    else
+      flash[:error] = "Sorry, this ingredient is the last ingredient in an existing recipe. It cannot be deleted."
+    end
+
     redirect_to user_dashboard_path(session[:user_id])
   end
 
