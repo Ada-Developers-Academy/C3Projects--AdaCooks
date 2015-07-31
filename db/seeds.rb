b85a386c7ca27c64164f60040d281d8adbc73ba4
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-30 19:44:41 -0700

    Added some seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # users ------------------------------------------------------------------------
 users_params = {
-  gurl: { username: "gurl", email: "gurl@example.com", password: "gurl", password_confirmation: "gurl", bio: "Just a midwest gurl." }
+  gurl: { username: "gurl", email: "gurl@example.com", password: "gurl", password_confirmation: "gurl", bio: "Just a midwest gurl." },
+  cheese_man: { username: "I LIKE CHEESE", email: "cheese@rulez.com", password: "potato", password_confirmation: "potato", bio: "I LIKE CHEESE, COULD YOU TELL!?!" },
+  apple_girl: { username: "AppleGirl", email: "apple@girl.com", password: "apple", password_confirmation: "apple" }
 }
 
 users = {}
@@ -12,7 +14,9 @@ end
 recipes_params = {
   apple_crisp: { name: "Apple Crisp", description: "Yummy, cinnamony goodness!", user_id: users[:gurl].id, image: "apple_crisp.jpg" },
   oreo_ice_cream: { name: "Oreo Ice Cream", description: "Delicious on a hot day!", user_id: users[:gurl].id, image: "oreo_ice_cream.jpg" },
-  brownies: { name: "Brownies", description: "Delicious on a cold day!", user_id: users[:gurl].id, image: "brownies.jpg" }
+  brownies: { name: "Brownies", description: "Delicious on a cold day!", user_id: users[:gurl].id, image: "brownies.jpg" },
+  mac_and_cheese: { name: "Mac & Cheese", description: "SO YUMMY!!", user_id: users[:cheese_man].id, image: "mac-cheese.jpg" },
+  water_recipe: { name: "Water", description: "Drink me.", user_id: users[:cheese_man].id }
 }
 
 recipes = {}
@@ -41,12 +45,18 @@ ingredients_params = {
   salt: { name: "Salt", user_id: users[:gurl].id },
   baking_powder: { name: "Baking Powder", user_id: users[:gurl].id },
   honey: { name: "Honey", user_id: users[:gurl].id },
-  powdered_sugar: { name: "Powdered Sugar", user_id: users[:gurl].id }
+  powdered_sugar: { name: "Powdered Sugar", user_id: users[:gurl].id },
+  cheese: { name: "Cheese", user_id: users[:cheese_man].id },
+  water: { name: "Water", user_id: users[:apple_girl].id, image: "water.jpg" }
 }
 
 ingredients = {}
 ingredients_params.each do |var_name, params|
   ingredients[var_name] = Ingredient.create(params)
+  if params[:image]
+    ingredients[var_name].image = Rails.root.join("db/seeds/images/#{params[:image]}").open
+    ingredients[var_name].save!
+  end
 end
 
 # steps ------------------------------------------------------------------------
@@ -70,6 +80,12 @@ brownies_step6 = Step.create( { recipe_id: recipes[:brownies].id, number: 6, dir
 brownies_step7 = Step.create( { recipe_id: recipes[:brownies].id, number: 7, directions: "Stir until smooth." } )
 brownies_step8 = Step.create( { recipe_id: recipes[:brownies].id, number: 8, directions: "Frost brownies while they are still warm!" } )
 brownies_step9 = Step.create( { recipe_id: recipes[:brownies].id, number: 9, directions: "After frosting brownies, put them back into the oven for 2 minutes to help melt/spread the frosting." } )
+
+mac_cheese_step1 = Step.create( { recipe_id: recipes[:mac_and_cheese].id, number: 1, directions: "Add cheese." } )
+mac_cheese_step2 = Step.create( { recipe_id: recipes[:mac_and_cheese].id, number: 2, directions: "Add more cheese." } )
+mac_cheese_step3 = Step.create( { recipe_id: recipes[:mac_and_cheese].id, number: 3, directions: "Be sure to add more cheese." } )
+
+water_step1 = Step.create( { recipe_id: recipes[:water_recipe].id, number: 1, directions: "Drink." } )
 
 # measurements -----------------------------------------------------------------
 measurements = [
@@ -101,7 +117,15 @@ measurements = [
   { recipe_id: recipes[:brownies].id, step_id: brownies_step5.id, ingredient_id: ingredients[:cocoa_powder].id, amount: "3 Tbsp" },
   { recipe_id: recipes[:brownies].id, step_id: brownies_step5.id, ingredient_id: ingredients[:honey].id, amount: "1 Tbsp" },
   { recipe_id: recipes[:brownies].id, step_id: brownies_step5.id, ingredient_id: ingredients[:vanilla].id, amount: "1 tsp" },
-  { recipe_id: recipes[:brownies].id, step_id: brownies_step5.id, ingredient_id: ingredients[:powdered_sugar].id, amount: "1 C" }
+  { recipe_id: recipes[:brownies].id, step_id: brownies_step5.id, ingredient_id: ingredients[:powdered_sugar].id, amount: "1 C" },
+
+  # cheese
+  { recipe_id: recipes[:mac_and_cheese].id, step_id: mac_cheese_step1.id, ingredient_id: ingredients[:cheese].id, amount: "1 Block" },
+  { recipe_id: recipes[:mac_and_cheese].id, step_id: mac_cheese_step2.id, ingredient_id: ingredients[:cheese].id, amount: "Another Block" },
+  { recipe_id: recipes[:mac_and_cheese].id, step_id: mac_cheese_step3.id, ingredient_id: ingredients[:cheese].id, amount: "All Blocks in Your House" },
+
+  # water
+  { recipe_id: recipes[:water_recipe].id, step_id: water_step1.id, ingredient_id: ingredients[:water].id, amount: "1" }
 ]
 
 measurements.each do |measurement|
@@ -111,9 +135,10 @@ end
 # cookbooks --------------------------------------------------------------------
 
 cookbooks_params = {
-  amazing_apple_appetizers: { name: "Amazing Apple Appetizers", description: "OMG APPLES ARE AMAZING!!!!111!!!!", user_id: users[:gurl].id },
+  amazing_apple_appetizers: { name: "Amazing Apple Appetizers", description: "OMG APPLES ARE AMAZING!!!!111!!!!", user_id: users[:apple_girl].id },
   food_for_thought: { name: "Food for Thought", description: "Think about this food before you eat it. Perhaps it will think back.", user_id: users[:gurl].id },
-  water_everything_you_can_cook_with_it: { name: "Water: Everything You Can Cook With It", description: "Water. The essence of life.", user_id: users[:gurl].id }
+  everything_cheese: { name: "EVERYTHING CHEESE", description: "DID YOU NOT READ THE TITLE!!", user_id: users[:cheese_man].id },
+  water_everything_you_can_cook_with_it: { name: "Water: Everything You Can Cook With It", description: "Water. The essence of life.", user_id: users[:cheese_man].id }
 }
 
 cookbooks = {}
@@ -125,3 +150,5 @@ cookbooks[:amazing_apple_appetizers].recipes << recipes[:apple_crisp]
 recipes.each_value do |object|
   cookbooks[:food_for_thought].recipes << object
 end
+cookbooks[:everything_cheese].recipes << recipes[:mac_and_cheese]
+cookbooks[:water_everything_you_can_cook_with_it].recipes << recipes[:water_recipe]
