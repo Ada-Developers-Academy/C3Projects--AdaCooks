bfda207094952b24d200d52c8876e228b622b142
MisShellyMac <michellemccarthy@hotmail.com>
2015-07-31 11:15:15 -0700

    WIP Specs

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -17,51 +17,19 @@ RSpec.describe RecipesController, type: :controller do
   end
 
   describe "GET #new" do
-   let(:recipe_params) do
-     {
-       recipe: {
-         name: "enchilada",
-         preparation: "bake til warm",
-         ingredient_ids: 2,
-         user_id: 1
-       }
-     }
-   end
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      @user = create :user
+      session[:user_id] = @user.id
+      get :new, user_id: @user.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
 
    it "creates a new Recipe object" do
      session[:user_id] = create(:user)
-     get :new, recipe_params
+     get :new
      expect(assigns(:recipe)).to be_kind_of(Object)
    end
  end
-end
-
-#    describe "GET 'new' " do
-#      let(:recipe_params) do
-#        {
-#          recipe: {
-#            name: "Yay",
-#            preparation: "Yup",
-#            user_id: 1
-#          }
-#        }
-#      end
-#
-#      it "renders the :new view" do
-#       get :new, recipe_params
-#       expect(response).to render_template("new")
-#     end
-#   end
-# end
-# #     let!(:recipe){Recipe.create(name: "Hot Tamale Chicken", description: "Corn Husks and Masa filled goodness", preparation: "Cook in oven til warm.", user_id: 1)}
-# #     it "renders the 'new' page" do
-#       get 'new', user_id: 1
-#       expect(response).to render_template("new")
-#     end
-#   end
-# end
-
-
-# def recipe_params
-#   params.require(:recipe).permit(:name, :description, :preparation, :image, :categories, :user_id)
-# end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe UsersController, type: :controller do
       get :new
     end
   end
+
+  describe "GET #Signup" do
+    it "responds successfully" do
+      get :new
+      expect(response).to have_http_status(200)
+      expect(response).to be_success
+      end
+    end
+
+  it "renders new template for signup" do
+      get :new
+      expect(response).to render_template("new")
+    end
+
+    describe "POST #create" do
+    context "valid form input" do
+      before :each do
+        @params = attributes_for :user
+      end
+    end
+
+  it "redirects to newly created user's show page" do
+      post :create, user: @params
+      expect(response).to have_http_status(302)
+    end
+  end
 end
