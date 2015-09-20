52a7688d51cabd1d39638d541888282d0e40062d
Anita <asovay@gmail.com>
2015-09-20 00:09:10 -0700

    debugging heroku deployment

--- db/migrate/20150727212659_create_recipes.rb
+++ db/migrate/20150727212659_create_recipes.rb
@@ -5,8 +5,8 @@ class CreateRecipes < ActiveRecord::Migration
       t.text :desc
       t.string :image
       t.text :prep
-      t.references :ingredient, index: true, foreign_key: true
-      t.references :cookbook, index: true, foreign_key: true
+      # t.references :ingredient, index: true, foreign_key: true
+      # t.references :cookbook, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/migrate/20150727212749_create_ingredients.rb
+++ db/migrate/20150727212749_create_ingredients.rb
@@ -3,8 +3,8 @@ class CreateIngredients < ActiveRecord::Migration
     create_table :ingredients do |t|
       t.string :name
       t.string :image
-      t.references :recipe, index: true, foreign_key: true
-      t.references :cookbook, index: true, foreign_key: true
+      # t.references :recipe, index: true, foreign_key: true
+      # t.references :cookbook, index: true, foreign_key: true
 
       t.timestamps null: false
     end
